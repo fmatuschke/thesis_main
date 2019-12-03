b02f08f010be698ad8ec39d8f155fa2575449835
fmatuschke <f.matuschke@fz-juelich.de>
2019-12-03 14:42:13 +0100

    ADD fastpli_helper and model_pop_2

---
+++ fastpli_helper.py
@@ -0,0 +1,61 @@
+import numpy as np
+import h5py
+import glob
+import os
+
+import fastpli.io
+
+
+def _pip_freeze():
+    try:
+        from pip._internal.operations import freeze
+    except ImportError:
+        from pip.operations import freeze
+    return "\n".join(freeze.freeze())
+
+
+def save_h5_fibers(h5_name,
+                   fiber_bundles,
+                   file_script,
+                   solver_dict=None,
+                   solver_step=None,
+                   solver_collisions=None,
+                   solver_overlap=None):
+
+    fastpli.io.fiber.save(h5_name, fiber_bundles, '/fiber_bundles', 'w-')
+    with h5py.File(h5_name, 'r+') as h5f:
+        h5f['/fiber_bundles'].attrs['version'] = fastpli.__version__
+        h5f['/fiber_bundles'].attrs['pip_freeze'] = _pip_freeze()
+        h5f['/fiber_bundles'].attrs['script'] = open(
+            os.path.abspath(file_script), 'r').read()
+
+        if solver_dict:
+            h5f['/fiber_bundles'].attrs['solver'] = str(solver_dict)
+        if solver_step:
+            h5f['/fiber_bundles'].attrs['solver.steps'] = solver_step
+        if solver_collisions:
+            h5f['/fiber_bundles'].attrs[
+                'solver.num_col_obj'] = solver_collisions
+        if solver_overlap:
+            h5f['/fiber_bundles'].attrs['solver.steps'] = solver_overlap
+
+
+def version_file_name(file_name):
+
+    file_path = os.path.dirname(file_name)
+    file_name = os.path.basename(file_name)
+    file_base = file_name.rpartition('.')[0]
+    file_ext = file_name.rpartition('.')[-1]
+    files = glob.glob(os.path.join(file_path, file_base + '*.' + file_ext))
+
+    def in_list(i, file):
+        for f in files:
+            if file_base + ".v{}".format(i) in f:
+                return True
+        return False
+
+    i = 0
+    while in_list(i, files):
+        i += 1
+
+    return os.path.join(file_path, file_base + ".v{}".format(i))
--- model_pop_2.py
+++ model_pop_2.py
@@ -9,69 +9,25 @@ import os
 from tqdm import tqdm
 from mpi4py import MPI
 
-
-def get_pip_freeze():
-    try:
-        from pip._internal.operations import freeze
-    except ImportError:
-        from pip.operations import freeze
-    return "\n".join(freeze.freeze())
-
-
-def save_fibers(file_name, fiber_bundles, solver_dict=None, i=0, n=0):
-    fastpli.io.fiber.save(file_name, fiber_bundles, '/fiber_bundles', 'w-')
-    with h5py.File(file_name, 'r+') as h5f:
-        h5f['/fiber_bundles'].attrs['version'] = fastpli.__version__
-        h5f['/fiber_bundles'].attrs['pip_freeze'] = get_pip_freeze()
-        with open(os.path.abspath(__file__), 'r') as f:
-            h5f['/fiber_bundles'].attrs['script'] = f.read()
-        if solver_dict:
-            h5f['/fiber_bundles'].attrs['solver'] = str(solver_dict)
-            h5f['/fiber_bundles'].attrs['solver.steps'] = i
-            h5f['/fiber_bundles'].attrs['solver.num_col_obj'] = n
-
+import fastpli_helper as helper
 
 # reproducability
 np.random.seed(42)
 
+# GLOBAL PARAMETERS
 FILE_NAME = os.path.abspath(__file__)
 FILE_PATH = os.path.dirname(FILE_NAME)
 FILE_BASE = os.path.basename(FILE_NAME)
 
-MODEL_NAME = "cube_2pop"
+MODEL_NAME = "cube_pop_2"
 OUTPUT_PATH = os.path.join(FILE_PATH, 'output', 'models')
 os.makedirs(OUTPUT_PATH, exist_ok=True)
 
-# PARAMETERS
-DPHI = np.linspace(0, 90, 5, True)
+# FIBERS
+DPHI = np.linspace(0, 90, 10, True)
 RADIUS_LOGMEAN = 1
+LENGTH = 65 * np.sqrt(3)
 PARAMETERS = DPHI
-
-ORG = 64
-LAP = 64
-PM = 1.28
-NUM_LAP_PIXEL = 1
-LENGTH = NUM_LAP_PIXEL * LAP * np.sqrt(3)
-
-
-def next_file_name():
-    import glob
-
-    name = os.path.join(OUTPUT_PATH, MODEL_NAME)
-    files = glob.glob(os.path.join(OUTPUT_PATH, MODEL_NAME + '*'))
-
-    def in_list(i, file):
-        for f in files:
-            if name + ".%s" % i in f:
-                return True
-        return False
-
-    i = 0
-    while in_list(i, files):
-        i += 1
-
-    return name + ".%s" % i
-
 
 # setup solver
 solver = fastpli.model.solver.Solver()
@@ -89,7 +45,7 @@ for rank, dphi in enumerate(PARAMETERS[comm.Get_rank()::comm.Get_size()]):
 
     seeds = fastpli.model.sandbox.seeds.triangular_grid(LENGTH,
                                                         LENGTH,
-                                                        2 * RADIUS_LOGMEAN,
+                                                        1.5 * RADIUS_LOGMEAN,
                                                         center=True)
     rnd_radii = RADIUS_LOGMEAN * np.random.lognormal(0, 0.1, seeds.shape[0])
 
@@ -112,9 +68,6 @@ for rank, dphi in enumerate(PARAMETERS[comm.Get_rank()::comm.Get_size()]):
     print("rank:" + str(comm.Get_rank()), "init:", solver.num_obj,
           solver.num_col_obj)
 
-    # solver.draw_scene()
-    # input()
-
     # check boundry to split fiber into segments
     for fb in fiber_bundles:
         for f in fb:
@@ -129,26 +82,30 @@ for rank, dphi in enumerate(PARAMETERS[comm.Get_rank()::comm.Get_size()]):
     print("rank:" + str(comm.Get_rank()), "init:", solver.num_obj,
           solver.num_col_obj)
 
-    # solver.draw_scene()
-    # input()
-
     # Save Data
-    file_pref = next_file_name() + 'dphi_' + str(dphi)
+    file_pref = helper.version_file_name(
+        os.path.join(OUTPUT_PATH, MODEL_NAME + '_dphi_' + str(dphi)))
+
     print(file_pref)
-    save_fibers(file_pref + '.init.h5', solver.fiber_bundles)
+
+    helper.save_h5_fibers(file_pref + '.init.h5', solver.fiber_bundles,
+                          __file__)
     fastpli.io.fiber.save(file_pref + '.init.dat', solver.fiber_bundles)
 
     # Run Solver
     for i in tqdm(range(1000)):
-        if i % 25 == 0:
-            tqdm.write("rank: {} step: {} {} {}".format(comm.Get_rank(), i,
-                                                        solver.num_obj,
-                                                        solver.num_col_obj))
         if solver.step():
             break
-    print("rank:" + str(comm.Get_rank()), "step:", i, solver.num_obj,
-          solver.num_col_obj)
 
-    save_fibers(file_pref + '.solved.h5', solver.fiber_bundles,
-                solver.as_dict(), i, solver.num_col_obj)
+        if i % 25 == 0:
+            tqdm.write("rank: {} step: {} {} {} {}%".format(
+                comm.Get_rank(), i, solver.num_obj, solver.num_col_obj,
+                round(solver.overlap / solver.num_col_obj * 100)))
+
+    print("rank: {} step: {} {}".format(comm.Get_rank(), i, solver.num_obj,
+                                        solver.num_col_obj))
+
+    helper.save_h5_fibers(file_pref + '.solved.h5', solver.fiber_bundles,
+                          __file__, solver.as_dict(), i, solver.num_col_obj,
+                          solver.overlap)
     fastpli.io.fiber.save(file_pref + '.solved.dat', solver.fiber_bundles)
