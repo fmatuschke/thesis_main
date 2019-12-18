b6abd97382b05a59730f5c8fefdbc17bc80eb807
fmatuschke <f.matuschke@fz-juelich.de>
2019-12-18 11:09:17 +0100

    ADD jureca_run.sh

---
+++ jureca_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#SBATCH --nodes=2
+#SBATCH --ntasks=24
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpu-per-task=2
+#SBATCH --output=mpi-out.%j
+#SBATCH --error=mpi-err.%j
+#SBATCH --time=10:00:00
+#SBATCH --partition=batch
+#SBATCH --mail-user=f.matuschke@fz-juelich.de
+#SBATCH --mail-type=ALL
+
+srun -n 24 env-jureca/bin/python3 -u model_2pop.py
