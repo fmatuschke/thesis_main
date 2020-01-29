7f36729390d03db2cb5fa2aef13b25671c16a546
fmatuschke <f.matuschke@fz-juelich.de>
2020-01-29 14:09:36 +0100

    WIP

--- cube_2pop.py
+++ cube_2pop.py
@@ -25,133 +25,144 @@ FILE_NAME = os.path.abspath(__file__)
 FILE_PATH = os.path.dirname(FILE_NAME)
 FILE_BASE = os.path.basename(FILE_NAME)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o",
-                    "--output",
-                    type=str,
-                    required=True,
-                    help="Output path of solver.")
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        comm.Abort()
 
-args = parser.parse_args()
-os.makedirs(os.path.join(args.output, 'models'), exist_ok=True)
 
-# logger
-logger = logging.getLogger("rank[%i]" % comm.rank)
-logger.setLevel(logging.DEBUG)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o",
+                        "--output",
+                        type=str,
+                        required=True,
+                        help="Output path of solver.")
 
-log_file = fastpli.tools.helper.version_file_name(
-    os.path.join(args.output, FILE_BASE + '.solver')) + ".log"
-mh = MPIFileHandler(log_file)
-formatter = logging.Formatter(
-    '%(asctime)s:%(name)s:%(levelname)s:\t%(message)s')
-mh.setFormatter(formatter)
-logger.addHandler(mh)
+    args = parser.parse_args()
+    os.makedirs(os.path.join(args.output, 'models'), exist_ok=True)
 
-# Fiber Model
-LENGTH = 120
-RADIUS_LOGMEAN = 1
-DPHI = np.linspace(0, 90, 10, True)
-PSI = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-PDPHI, PPSI = np.meshgrid(DPHI, PSI)
-PARAMETER = list(zip(PDPHI.flatten(), PPSI.flatten()))
-PARAMETER.append((0.0, 0.0))
-PARAMETER.append((0.0, 1.0))
+    # logger
+    logger = logging.getLogger("rank[%i]" % comm.rank)
+    logger.setLevel(logging.DEBUG)
 
-# solve
-for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
-    logger.info(f"dphi:{dphi}, psi:{psi}")
+    log_file = fastpli.tools.helper.version_file_name(
+        os.path.join(args.output, FILE_BASE + '.solver')) + ".log"
+    mh = MPIFileHandler(log_file)
+    formatter = logging.Formatter(
+        '%(asctime)s:%(name)s:%(levelname)s:\t%(message)s')
+    mh.setFormatter(formatter)
+    logger.addHandler(mh)
 
-    # setup solver
-    solver = fastpli.model.solver.Solver()
-    solver.obj_mean_length = RADIUS_LOGMEAN * 2
-    solver.obj_min_radius = RADIUS_LOGMEAN * 5
-    solver.omp_num_threads = NUM_THREADS
+    # Fiber Model
+    LENGTH = 120
+    RADIUS_LOGMEAN = 1
+    DPHI = np.linspace(0, 90, 10, True)
+    PSI = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    PDPHI, PPSI = np.meshgrid(DPHI, PSI)
+    PARAMETER = list(zip(PDPHI.flatten(), PPSI.flatten()))
+    PARAMETER.append((0.0, 0.0))
+    PARAMETER.append((0.0, 1.0))
 
-    file_pref = fastpli.tools.helper.version_file_name(
-        os.path.join(args.output, 'models', FILE_BASE + '_dphi_' +
-                     str(round(dphi, 1))) + '_psi_' + str(round(psi, 1)))
-    logger.info(f"file_pref: {file_pref}")
+    # solve
+    for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
+        logger.info(f"dphi:{dphi}, psi:{psi}")
 
-    seeds = fastpli.model.sandbox.seeds.triangular_grid(LENGTH * 2,
-                                                        LENGTH * 2,
-                                                        2 * RADIUS_LOGMEAN,
-                                                        center=True)
+        # setup solver
+        solver = fastpli.model.solver.Solver()
+        solver.obj_mean_length = RADIUS_LOGMEAN * 2
+        solver.obj_min_radius = RADIUS_LOGMEAN * 5
+        solver.omp_num_threads = NUM_THREADS
 
-    # pick random seeds for volume distribution
-    seeds_0 = seeds[np.random.rand(seeds.shape[0]) < psi, :]
-    seeds_1 = seeds[np.random.rand(seeds.shape[0]) < (1 - psi), :]
-    rnd_radii_0 = RADIUS_LOGMEAN * np.random.lognormal(0, 0.1, seeds_0.shape[0])
-    rnd_radii_1 = RADIUS_LOGMEAN * np.random.lognormal(0, 0.1, seeds_1.shape[0])
+        file_pref = fastpli.tools.helper.version_file_name(
+            os.path.join(args.output, 'models', FILE_BASE + '_dphi_' +
+                         str(round(dphi, 1))) + '_psi_' + str(round(psi, 1)))
+        logger.info(f"file_pref: {file_pref}")
 
-    fiber_bundles = [
-        fastpli.model.sandbox.build.cuboid(
-            p=-0.5 * np.array([LENGTH, LENGTH, LENGTH]),
-            q=0.5 * np.array([LENGTH, LENGTH, LENGTH]),
-            phi=np.deg2rad(0),
-            theta=np.deg2rad(90),
-            seeds=seeds_0,
-            radii=rnd_radii_0),
-        fastpli.model.sandbox.build.cuboid(
-            p=-0.5 * np.array([LENGTH, LENGTH, LENGTH]),
-            q=0.5 * np.array([LENGTH, LENGTH, LENGTH]),
-            phi=np.deg2rad(dphi),
-            theta=np.deg2rad(90),
-            seeds=seeds_1 + RADIUS_LOGMEAN,
-            radii=rnd_radii_1)
-    ]
+        seeds = fastpli.model.sandbox.seeds.triangular_grid(LENGTH * 2,
+                                                            LENGTH * 2,
+                                                            2 * RADIUS_LOGMEAN,
+                                                            center=True)
 
-    solver.fiber_bundles = fiber_bundles
-    fiber_bundles = solver.apply_boundary_conditions(100)
-    logger.info(f"init: {solver.num_obj}/{solver.num_col_obj}")
+        # pick random seeds for volume distribution
+        seeds_0 = seeds[np.random.rand(seeds.shape[0]) < psi, :]
+        seeds_1 = seeds[np.random.rand(seeds.shape[0]) < (1 - psi), :]
+        rnd_radii_0 = RADIUS_LOGMEAN * np.random.lognormal(
+            0, 0.1, seeds_0.shape[0])
+        rnd_radii_1 = RADIUS_LOGMEAN * np.random.lognormal(
+            0, 0.1, seeds_1.shape[0])
 
-    # add rnd displacement
-    for fb in fiber_bundles:
-        for f in fb:
-            f[:, :-1] += np.random.normal(0, 0.05 * RADIUS_LOGMEAN,
-                                          (f.shape[0], 3))
-            f[:, -1] *= np.random.lognormal(0, 0.05, f.shape[0])
+        fiber_bundles = [
+            fastpli.model.sandbox.build.cuboid(
+                p=-0.5 * np.array([LENGTH, LENGTH, LENGTH]),
+                q=0.5 * np.array([LENGTH, LENGTH, LENGTH]),
+                phi=np.deg2rad(0),
+                theta=np.deg2rad(90),
+                seeds=seeds_0,
+                radii=rnd_radii_0),
+            fastpli.model.sandbox.build.cuboid(
+                p=-0.5 * np.array([LENGTH, LENGTH, LENGTH]),
+                q=0.5 * np.array([LENGTH, LENGTH, LENGTH]),
+                phi=np.deg2rad(dphi),
+                theta=np.deg2rad(90),
+                seeds=seeds_1 + RADIUS_LOGMEAN,
+                radii=rnd_radii_1)
+        ]
 
-    solver.fiber_bundles = fiber_bundles
-    fiber_bundles = solver.apply_boundary_conditions(100)
-    logger.info(f"rnd displacement: {solver.num_obj}/{solver.num_col_obj}")
+        solver.fiber_bundles = fiber_bundles
+        fiber_bundles = solver.apply_boundary_conditions(100)
+        logger.info(f"init: {solver.num_obj}/{solver.num_col_obj}")
 
-    # if comm.Get_rank() == 0:
-    #     solver.draw_scene()
+        # add rnd displacement
+        for fb in fiber_bundles:
+            for f in fb:
+                f[:, :-1] += np.random.normal(0, 0.05 * RADIUS_LOGMEAN,
+                                              (f.shape[0], 3))
+                f[:, -1] *= np.random.lognormal(0, 0.05, f.shape[0])
 
-    # Save Data
-    logger.debug(f"save init")
-    fastpli.tools.helper.save_h5_fibers(file_pref + '.init.h5',
-                                        solver.fiber_bundles, 'fiber_bundles',
-                                        __file__)
-    fastpli.io.fiber.save(file_pref + '.init.dat', solver.fiber_bundles)
+        solver.fiber_bundles = fiber_bundles
+        fiber_bundles = solver.apply_boundary_conditions(100)
+        logger.info(f"rnd displacement: {solver.num_obj}/{solver.num_col_obj}")
 
-    # Run Solver
-    logger.info(f"run solver")
-    for i in tqdm(range(10000)):
-        if solver.step():
-            break
+        # if comm.Get_rank() == 0:
+        #     solver.draw_scene()
 
-        overlap = solver.overlap / solver.num_col_obj if solver.num_col_obj else 0
-        if i % 50 == 0:
-            logger.info(
-                f"step: {i}, {solver.num_obj}/{solver.num_col_obj} {round(overlap * 100)}%"
-            )
+        # Save Data
+        logger.debug(f"save init")
+        fastpli.tools.helper.save_h5_fibers(file_pref + '.init.h5',
+                                            solver.fiber_bundles,
+                                            'fiber_bundles', __file__)
+        fastpli.io.fiber.save(file_pref + '.init.dat', solver.fiber_bundles)
 
-        if overlap <= 0.01:
-            break
+        # Run Solver
+        logger.info(f"run solver")
+        for i in tqdm(range(10000)):
+            if solver.step():
+                break
 
-    logger.info(f"solved: {i}, {solver.num_obj}/{solver.num_col_obj}")
+            overlap = solver.overlap / solver.num_col_obj if solver.num_col_obj else 0
+            if i % 50 == 0:
+                logger.info(
+                    f"step: {i}, {solver.num_obj}/{solver.num_col_obj} {round(overlap * 100)}%"
+                )
 
-    logger.debug(f"save solved")
-    fastpli.tools.helper.save_h5_fibers(file_pref + '.solved.h5',
-                                        solver.fiber_bundles, 'fiber_bundles',
-                                        __file__, solver.get_dict(), i,
-                                        solver.num_col_obj, solver.overlap)
+            if overlap <= 0.01:
+                break
 
-    logger.debug(f"save solved attrs")
-    h5f = h5py.File(file_pref + '.solved.h5', 'r+')
-    h5f['fiber_bundles'].attrs['psi'] = psi
-    h5f['fiber_bundles'].attrs['dphi'] = dphi
+        logger.info(f"solved: {i}, {solver.num_obj}/{solver.num_col_obj}")
 
-    logger.debug(f"save solved dat")
-    fastpli.io.fiber.save(file_pref + '.solved.dat', solver.fiber_bundles)
+        logger.debug(f"save solved")
+        fastpli.tools.helper.save_h5_fibers(file_pref + '.solved.h5',
+                                            solver.fiber_bundles,
+                                            'fiber_bundles', __file__,
+                                            solver.get_dict(), i,
+                                            solver.num_col_obj, solver.overlap)
+
+        logger.debug(f"save solved attrs")
+        h5f = h5py.File(file_pref + '.solved.h5', 'r+')
+        h5f['fiber_bundles'].attrs['psi'] = psi
+        h5f['fiber_bundles'].attrs['dphi'] = dphi
+
+        logger.debug(f"save solved dat")
+        fastpli.io.fiber.save(file_pref + '.solved.dat', solver.fiber_bundles)
--- simulation.py
+++ simulation.py
@@ -28,151 +28,161 @@ FILE_NAME = os.path.abspath(__file__)
 FILE_PATH = os.path.dirname(FILE_NAME)
 FILE_BASE = os.path.basename(FILE_NAME)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o",
-                    "--output",
-                    type=str,
-                    required=True,
-                    help="Output path of solver.")
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        comm.Abort()
 
-parser.add_argument("-i",
-                    "--input",
-                    type=str,
-                    required=True,
-                    help="input regex string.")
 
-args = parser.parse_args()
-os.makedirs(os.path.join(args.output, 'simulations'), exist_ok=True)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o",
+                        "--output",
+                        type=str,
+                        required=True,
+                        help="Output path of solver.")
 
-# logger
-logger = logging.getLogger("rank[%i]" % comm.rank)
-logger.setLevel(logging.DEBUG)
+    parser.add_argument("-i",
+                        "--input",
+                        type=str,
+                        required=True,
+                        help="input regex string.")
 
-log_file = fastpli.tools.helper.version_file_name(
-    os.path.join(args.output, 'simulation')) + ".log"
-mh = MPIFileHandler(log_file)
-formatter = logging.Formatter(
-    '%(asctime)s:%(name)s:%(levelname)s:\t%(message)s')
-mh.setFormatter(formatter)
-logger.addHandler(mh)
+    args = parser.parse_args()
+    os.makedirs(os.path.join(args.output, 'simulations'), exist_ok=True)
 
-#%% PARAMETER
-VOXEL_SIZE = 0.1
-LENGTH = 65
-THICKNESS = 60
+    # logger
+    logger = logging.getLogger("rank[%i]" % comm.rank)
+    logger.setLevel(logging.DEBUG)
 
-file_list = sorted(glob.glob(os.path.join(args.output, 'models', args.input)))
-if not file_list:
-    logger.error("no files detected")
-    comm.Abort()
-    sys.exit()
+    log_file = fastpli.tools.helper.version_file_name(
+        os.path.join(args.output, 'simulation')) + ".log"
+    mh = MPIFileHandler(log_file)
+    formatter = logging.Formatter(
+        '%(asctime)s:%(name)s:%(levelname)s:\t%(message)s')
+    mh.setFormatter(formatter)
+    logger.addHandler(mh)
 
-# print Memory
-simpli = fastpli.simulation.Simpli()
-simpli.voxel_size = VOXEL_SIZE  # in mu meter
-simpli.set_voi(-0.5 * np.array([65, 65, 60]),
-               0.5 * np.array([65, 65, 60]))  # in mu meter
+    # PARAMETER
+    VOXEL_SIZE = 0.1
+    LENGTH = 65
+    THICKNESS = 60
 
-logger.info(f"Single Memory: {round(simpli.memory_usage())} MB")
-logger.info(f"Total Memory: {round(simpli.memory_usage()* comm.Get_size())} MB")
-del simpli
+    file_list = sorted(
+        glob.glob(os.path.join(args.output, 'models', args.input)))
+    if not file_list:
+        logger.error("no files detected")
 
-# simulation loop
-for file in tqdm(file_list[comm.Get_rank()::comm.Get_size()]):
-    logger.info(f"input file: {file}")
+    # print Memory
+    simpli = fastpli.simulation.Simpli()
+    simpli.voxel_size = VOXEL_SIZE  # in mu meter
+    simpli.set_voi(-0.5 * np.array([65, 65, 60]),
+                   0.5 * np.array([65, 65, 60]))  # in mu meter
 
-    # Loading fiber models and prepair rotations
-    fiber_bundles = fastpli.io.fiber.load(file, 'fiber_bundles')
+    logger.info(f"Single Memory: {round(simpli.memory_usage())} MB")
+    logger.info(
+        f"Total Memory: {round(simpli.memory_usage()* comm.Get_size())} MB")
+    del simpli
 
-    with h5py.File(file, 'r') as h5f:
-        psi = h5f['fiber_bundles'].attrs["psi"]
-        dphi = h5f['fiber_bundles'].attrs["dphi"]
+    # simulation loop
+    for file in tqdm(file_list[comm.Get_rank()::comm.Get_size()]):
+        logger.info(f"input file: {file}")
 
-    n_rot = int(
-        np.round(
-            np.sqrt((1 - np.cos(2 * np.deg2rad(dphi))) /
-                    (1 - np.cos(np.deg2rad(10))))))
+        # Loading fiber models and prepair rotations
+        fiber_bundles = fastpli.io.fiber.load(file, 'fiber_bundles')
 
-    if n_rot > 0:
-        n_rot = max(n_rot, 2)
+        with h5py.File(file, 'r') as h5f:
+            psi = h5f['fiber_bundles'].attrs["psi"]
+            dphi = h5f['fiber_bundles'].attrs["dphi"]
 
-    fiber_phi_rotations = np.linspace(0, 90, n_rot, True)
-    logger.info(f"dphi: {dphi}, n_rot: {n_rot}")
+        n_rot = int(
+            np.round(
+                np.sqrt((1 - np.cos(2 * np.deg2rad(dphi))) /
+                        (1 - np.cos(np.deg2rad(10))))))
 
-    if n_rot == 0:
-        fiber_phi_rotations = [0]
+        if n_rot > 0:
+            n_rot = max(n_rot, 2)
 
-    for f_phi in fiber_phi_rotations:
-        _, file_name = os.path.split(file)
-        file_name = os.path.splitext(file_name)[0]
-        file_name += '_phi_{:.2f}'.format(f_phi)
-        file_name = fastpli.tools.helper.version_file_name(
-            os.path.join(args.output, 'simulations', file_name))
+        fiber_phi_rotations = np.linspace(0, 90, n_rot, True)
+        logger.info(f"dphi: {dphi}, n_rot: {n_rot}")
 
-        logger.info(f"rotation : {f_phi}")
-        logger.info(f"output file: {file_name}")
+        if n_rot == 0:
+            fiber_phi_rotations = [0]
 
-        rot = fastpli.tools.rotation.x(np.deg2rad(f_phi))
+        for f_phi in fiber_phi_rotations:
+            _, file_name = os.path.split(file)
+            file_name = os.path.splitext(file_name)[0]
+            file_name += '_phi_{:.2f}'.format(f_phi)
+            file_name = fastpli.tools.helper.version_file_name(
+                os.path.join(args.output, 'simulations', file_name))
 
-        with h5py.File(file_name + '.h5', 'w-') as h5f:
-            with open(os.path.abspath(__file__), 'r') as script:
-                h5f['parameter/script'] = script.read()
-            h5f['parameter/input_file'] = file
+            logger.info(f"rotation : {f_phi}")
+            logger.info(f"output file: {file_name}")
 
-            for m, (dn, model) in enumerate([(-0.001, 'p'), (0.002, 'r')]):
-                for name, gain, intensity, res, tilt_angle in [
-                    ('LAP', 3, 26000, 20, 5.5), ('PM', 1.5, 50000, 1.25, 3.9)
-                ]:
-                    dset = h5f.create_group(name + '/' + model)
+            rot = fastpli.tools.rotation.x(np.deg2rad(f_phi))
 
-                    # Setup Simpli
-                    simpli = fastpli.simulation.Simpli()
-                    simpli.omp_num_threads = NUM_THREADS
-                    simpli.voxel_size = VOXEL_SIZE
-                    simpli.resolution = res
-                    simpli.filter_rotations = np.deg2rad(
-                        [0, 30, 60, 90, 120, 150])
-                    simpli.interpolate = True
-                    simpli.wavelength = 525  # in nm
-                    simpli.optical_sigma = 0.71  # in pixel size
-                    simpli.verbose = 0
+            with h5py.File(file_name + '.h5', 'w-') as h5f:
+                with open(os.path.abspath(__file__), 'r') as script:
+                    h5f['parameter/script'] = script.read()
+                h5f['parameter/input_file'] = file
 
-                    simpli.set_voi(-0.5 * np.array([LENGTH, LENGTH, THICKNESS]),
-                                   0.5 * np.array([LENGTH, LENGTH, THICKNESS]))
-                    simpli.tilts = np.deg2rad(
-                        np.array([(0, 0), (5.5, 0), (5.5, 90), (5.5, 180),
-                                  (5.5, 270)]))
-                    simpli.add_crop_tilt_halo()
+                for m, (dn, model) in enumerate([(-0.001, 'p'), (0.002, 'r')]):
+                    for name, gain, intensity, res, tilt_angle in [
+                        ('LAP', 3, 26000, 20, 5.5),
+                        ('PM', 1.5, 50000, 1.25, 3.9)
+                    ]:
+                        dset = h5f.create_group(name + '/' + model)
 
-                    simpli.fiber_bundles = fastpli.objects.fiber_bundles.Rotate(
-                        copy.deepcopy(fiber_bundles), rot)
-                    simpli.fiber_bundles_properties = [[(0.75, 0, 0, 'b'),
-                                                        (1.0, dn, 1, model)]
-                                                      ] * len(fiber_bundles)
+                        # Setup Simpli
+                        simpli = fastpli.simulation.Simpli()
+                        simpli.omp_num_threads = NUM_THREADS
+                        simpli.voxel_size = VOXEL_SIZE
+                        simpli.resolution = res
+                        simpli.filter_rotations = np.deg2rad(
+                            [0, 30, 60, 90, 120, 150])
+                        simpli.interpolate = True
+                        simpli.wavelength = 525  # in nm
+                        simpli.optical_sigma = 0.71  # in pixel size
+                        simpli.verbose = 0
 
-                    logger.info(f"tissue_pipeline: model:{model}")
-                    label_field, vector_field, tissue_properties = simpli.run_tissue_pipeline(
-                        h5f=dset, save=["label_field"])
+                        simpli.set_voi(
+                            -0.5 * np.array([LENGTH, LENGTH, THICKNESS]),
+                            0.5 * np.array([LENGTH, LENGTH, THICKNESS]))
+                        simpli.tilts = np.deg2rad(
+                            np.array([(0, 0), (5.5, 0), (5.5, 90), (5.5, 180),
+                                      (5.5, 270)]))
+                        simpli.add_crop_tilt_halo()
 
-                    # Simulate PLI Measurement
-                    logger.info(f"simulation_pipeline: model:{model}")
-                    # TODO: LAP run multiple pixels for statistics
+                        simpli.fiber_bundles = fastpli.objects.fiber_bundles.Rotate(
+                            copy.deepcopy(fiber_bundles), rot)
+                        simpli.fiber_bundles_properties = [[(0.75, 0, 0, 'b'),
+                                                            (1.0, dn, 1, model)]
+                                                          ] * len(fiber_bundles)
 
-                    simpli.light_intensity = intensity  # a.u.
-                    simpli.sensor_gain = gain
+                        logger.info(f"tissue_pipeline: model:{model}")
+                        label_field, vector_field, tissue_properties = simpli.run_tissue_pipeline(
+                            h5f=dset, save=["label_field"])
 
-                    simpli.save_parameter(h5f=dset)
-                    simpli.run_simulation_pipeline(label_field,
-                                                   vector_field,
-                                                   tissue_properties,
-                                                   h5f=dset,
-                                                   crop_tilt=True,
-                                                   mp_pool=mp_pool)
+                        # Simulate PLI Measurement
+                        logger.info(f"simulation_pipeline: model:{model}")
+                        # TODO: LAP run multiple pixels for statistics
 
-                    dset['parameter/model/psi'] = psi
-                    dset['parameter/model/dphi'] = dphi
-                    dset['parameter/model/phi'] = f_phi
+                        simpli.light_intensity = intensity  # a.u.
+                        simpli.sensor_gain = gain
 
-                    del label_field
-                    del vector_field
-                    del simpli
+                        simpli.save_parameter(h5f=dset)
+                        simpli.run_simulation_pipeline(label_field,
+                                                       vector_field,
+                                                       tissue_properties,
+                                                       h5f=dset,
+                                                       crop_tilt=True,
+                                                       mp_pool=mp_pool)
+
+                        dset['parameter/model/psi'] = psi
+                        dset['parameter/model/dphi'] = dphi
+                        dset['parameter/model/phi'] = f_phi
+
+                        del label_field
+                        del vector_field
+                        del simpli
