2fb2f5d74d29b12402c7b7f639e8196086569bb5
fmatuschke <f.matuschke@fz-juelich.de>
2020-01-29 14:46:29 +0100

    TEST

--- jureca_run.sh
+++ jureca_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-#SBATCH --nodes=1
-#SBATCH --ntasks=12
+#SBATCH --nodes=8
+#SBATCH --ntasks=96
 #SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=4
 #SBATCH --output=mpi-out.%j
@@ -10,5 +10,5 @@
 #SBATCH --mail-user=f.matuschke@fz-juelich.de
 #SBATCH --mail-type=ALL
 
-srun -n 12 env-jureca/bin/python3 -u -m mpi4py model_2pop.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output
-srun -n 12 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output -i "*v0.solved*.h5"
+srun -n 96 env-jureca/bin/python3 -u -m mpi4py cube_2pop.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output
+srun -n 96 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output -i "*v0.solved*.h5"
