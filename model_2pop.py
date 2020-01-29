b8d57c6687caeb927f9b73266112ac52d6501ca5
fmatuschke <f.matuschke@fz-juelich.de>
2020-01-29 09:23:39 +0100

    ADD logging and argparse

--- model_2pop.py
+++ model_2pop.py
@@ -1,35 +1,53 @@
+#%% import
 import fastpli.model.solver
 import fastpli.model.sandbox
 import fastpli.tools
 import fastpli.io
 
 import numpy as np
+import argparse
+import logging
 import h5py
 import sys
 import os
 
+from MPIFileHandler import MPIFileHandler
 from tqdm import tqdm
 from mpi4py import MPI
+comm = MPI.COMM_WORLD
 
 # reproducability
 np.random.seed(42)
 
-# GLOBAL PARAMETERS
+#%% path
 FILE_NAME = os.path.abspath(__file__)
 FILE_PATH = os.path.dirname(FILE_NAME)
 FILE_BASE = os.path.basename(FILE_NAME)
-
 MODEL_NAME = "cube_2pop"
 
-OUTPUT_PATH = os.path.join(FILE_PATH, 'models')
-if len(sys.argv) > 1:
-    OUTPUT_PATH = os.path.join(sys.argv[1], 'models')
-else:
-    raise ValueError("No input")
+parser = argparse.ArgumentParser()
+parser.add_argument("-o",
+                    "--output",
+                    type=str,
+                    required=True,
+                    help="Output path of solver.")
 
-os.makedirs(OUTPUT_PATH, exist_ok=True)
+args = parser.parse_args()
+os.makedirs(os.path.join(args.output, 'models'), exist_ok=True)
 
-# FIBERS
+#%% logger
+logger = logging.getLogger("rank[%i]" % comm.rank)
+logger.setLevel(logging.DEBUG)
+
+log_file = fastpli.tools.helper.version_file_name(
+    os.path.join(args.output, MODEL_NAME)) + ".log"
+mh = MPIFileHandler(log_file)
+formatter = logging.Formatter(
+    '%(asctime)s:%(name)s:%(levelname)s:\t%(message)s')
+mh.setFormatter(formatter)
+logger.addHandler(mh)
+
+#%% Fiber Model
 LENGTH = 120
 RADIUS_LOGMEAN = 1
 DPHI = np.linspace(0, 90, 10, True)
@@ -39,14 +57,9 @@ PARAMETER = list(zip(PDPHI.flatten(), PPSI.flatten()))
 PARAMETER.append((0.0, 0.0))
 PARAMETER.append((0.0, 1.0))
 
-# mpi
-comm = MPI.COMM_WORLD
-
-print(PDPHI.size)
-
-for dphi, psi in PARAMETER[comm.Get_rank()::comm.Get_size()]:
-
-    print(dphi, psi)
+#%% solve
+for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
+    logger.info(f"dphi:{dphi}, psi:{psi}")
 
     # setup solver
     solver = fastpli.model.solver.Solver()
@@ -55,16 +68,14 @@ for dphi, psi in PARAMETER[comm.Get_rank()::comm.Get_size()]:
     solver.omp_num_threads = 4
 
     file_pref = fastpli.tools.helper.version_file_name(
-        os.path.join(OUTPUT_PATH, MODEL_NAME + '_dphi_' + str(round(dphi, 1))) +
-        '_psi_' + str(round(psi, 1)))
-    print("rank {}: {}".format(str(comm.Get_rank()), file_pref))
+        os.path.join(args.output, 'models', MODEL_NAME + '_dphi_' +
+                     str(round(dphi, 1))) + '_psi_' + str(round(psi, 1)))
+    logger.info(f"file_pref: {file_pref}")
 
     seeds = fastpli.model.sandbox.seeds.triangular_grid(LENGTH * 2,
                                                         LENGTH * 2,
                                                         2 * RADIUS_LOGMEAN,
                                                         center=True)
-
-    # print(np.mean(rnd_radii), np.std(rnd_radii))
 
     # pick random seeds for volume distribution
     seeds_0 = seeds[np.random.rand(seeds.shape[0]) < psi, :]
@@ -91,11 +102,9 @@ for dphi, psi in PARAMETER[comm.Get_rank()::comm.Get_size()]:
 
     solver.fiber_bundles = fiber_bundles
     fiber_bundles = solver.apply_boundary_conditions(100)
+    logger.info(f"init: {solver.num_obj}/{solver.num_col_obj}")
 
-    print("rank:" + str(comm.Get_rank()), "init:", solver.num_obj,
-          solver.num_col_obj)
-
-    # check boundry to split fiber into segments
+    # add rnd displacement
     for fb in fiber_bundles:
         for f in fb:
             f[:, :-1] += np.random.normal(0, 0.05 * RADIUS_LOGMEAN,
@@ -104,44 +113,45 @@ for dphi, psi in PARAMETER[comm.Get_rank()::comm.Get_size()]:
 
     solver.fiber_bundles = fiber_bundles
     fiber_bundles = solver.apply_boundary_conditions(100)
+    logger.info(f"rnd displacement: {solver.num_obj}/{solver.num_col_obj}")
 
     # if comm.Get_rank() == 0:
     #     solver.draw_scene()
 
-    print("rank:" + str(comm.Get_rank()), "init:", solver.num_obj,
-          solver.num_col_obj)
-
     # Save Data
+    logger.debug(f"save init")
     fastpli.tools.helper.save_h5_fibers(file_pref + '.init.h5',
                                         solver.fiber_bundles, 'fiber_bundles',
                                         __file__)
     fastpli.io.fiber.save(file_pref + '.init.dat', solver.fiber_bundles)
 
     # Run Solver
-    for i in tqdm(range(10000)):
+    logger.info(f"run solver")
+    for i in tqdm(range(100)):
         if solver.step():
             break
 
         overlap = solver.overlap / solver.num_col_obj if solver.num_col_obj else 0
         if i % 50 == 0:
-            tqdm.write("rank: {} step: {} {} {} {}%".format(
-                comm.Get_rank(), i, solver.num_obj, solver.num_col_obj,
-                round(overlap * 100)))
+            logger.info(
+                f"step: {i}, {solver.num_obj}/{solver.num_col_obj} {round(overlap * 100)}%"
+            )
 
         if overlap <= 0.01:
             break
 
-    print("rank: {} step: {} {} {}".format(comm.Get_rank(), i, solver.num_obj,
-                                           solver.num_col_obj))
+    logger.info(f"solved: {i}, {solver.num_obj}/{solver.num_col_obj}")
 
+    logger.debug(f"save solved")
     fastpli.tools.helper.save_h5_fibers(file_pref + '.solved.h5',
-                                        solver.fiber_bundles,
-                                        'fiber_bundles', __file__,
-                                        solver.as_dict(), i, solver.num_col_obj,
-                                        solver.overlap)
+                                        solver.fiber_bundles, 'fiber_bundles',
+                                        __file__, solver.get_dict(), i,
+                                        solver.num_col_obj, solver.overlap)
 
+    logger.debug(f"save solved attrs")
     h5f = h5py.File(file_pref + '.solved.h5', 'r+')
     h5f['fiber_bundles'].attrs['psi'] = psi
     h5f['fiber_bundles'].attrs['dphi'] = dphi
 
+    logger.debug(f"save solved dat")
     fastpli.io.fiber.save(file_pref + '.solved.dat', solver.fiber_bundles)
