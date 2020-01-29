735a06b4c140c6f669386672b6bf58b97def5da9
fmatuschke <f.matuschke@fz-juelich.de>
2020-01-29 14:19:24 +0100

    WIP

--- jureca_run.sh
+++ jureca_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
-#SBATCH --nodes=8
-#SBATCH --ntasks=96
+#SBATCH --nodes=1
+#SBATCH --ntasks=12
 #SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=4
 #SBATCH --output=mpi-out.%j
 #SBATCH --error=mpi-err.%j
-#SBATCH --time=4:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=mem512
 #SBATCH --mail-user=f.matuschke@fz-juelich.de
 #SBATCH --mail-type=ALL
 
-srun -n 96 env-jureca/bin/python3 -u -m mpi4py model_2pop.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output
-srun -n 96 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output -i "*v0.solved*.h5"
+srun -n 12 env-jureca/bin/python3 -u -m mpi4py model_2pop.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output
+srun -n 12 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output -i "*v0.solved*.h5"
