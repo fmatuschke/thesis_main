41871541742605921fd741da75206270a1071480
fmatuschke <f.matuschke@fz-juelich.de>
2020-02-03 11:02:45 +0100

    FIX jureca_run

--- jureca_run.sh
+++ jureca_run.sh
@@ -11,4 +11,4 @@
 #SBATCH --mail-type=ALL
 
 srun -n 96 env-jureca/bin/python3 -u -m mpi4py cube_2pop.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output/models -n 10000
-srun -n 96 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output -i "*solved*.h5"
+srun -n 96 env-jureca/bin/python3 -u -m mpi4py simulation.py -o /p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output/simulations -i "/p/scratch/cjinm11/matuschke1/thesis/2_cube_factory/output/models/*solved*.h5"
