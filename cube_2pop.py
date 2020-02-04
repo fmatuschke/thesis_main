e2b82ec640818b701bb13803c9afc3a45fe8ed97
fmatuschke <f.matuschke@fz-juelich.de>
2020-02-04 16:41:28 +0100

    RFC cube_2pop

--- cube_2pop.py
+++ cube_2pop.py
@@ -61,18 +61,20 @@ logger.addHandler(mh)
 logger.info("args: " + " ".join(sys.argv[1:]))
 
 # Fiber Model
-LENGTH = 120
+LENGTH = 210  # to rotate a 120 um cube inside
 RADIUS_LOGMEAN = args.fiber_radius
-DPHI = np.linspace(0, 90, 10, True)
-PSI = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-PDPHI, PPSI = np.meshgrid(DPHI, PSI)
-PARAMETER = list(zip(PDPHI.flatten(), PPSI.flatten()))
+PSI = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,
+       0.9]  # fiber fraction: PSI * f0 + (1-PSI) * f1
+OMEGA = np.linspace(0, 90, 10, True)  # angle of opening (f0, f1)
+PSI, OMEGA = np.meshgrid(PSI, OMEGA)
+PARAMETER = list(zip(PSI.flatten(), OMEGA.flatten()))
 PARAMETER.append((0.0, 0.0))
-PARAMETER.append((0.0, 1.0))
+PARAMETER.append((1.0, 0.0))
 
 # solve
-for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
-    logger.info(f"dphi:{dphi}, psi:{psi}")
+PARAMETER = PARAMETER[:2]
+for psi, omega in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
+    logger.info(f"psi:{psi}, omega:{omega}")
 
     # setup solver
     solver = fastpli.model.solver.Solver()
@@ -80,8 +82,8 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
     solver.obj_min_radius = RADIUS_LOGMEAN * 5
     solver.omp_num_threads = NUM_THREADS
 
-    file_pref = output_name + '_dphi_' + str(round(dphi, 1)) + '_psi_' + str(
-        round(psi, 1))
+    file_pref = output_name + '_psi_' + str(round(psi, 2)) + '_omega_' + str(
+        round(omega, 2))
     logger.info(f"file_pref: {file_pref}")
 
     seeds = fastpli.model.sandbox.seeds.triangular_grid(LENGTH * 2,
@@ -89,7 +91,7 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
                                                         2 * RADIUS_LOGMEAN,
                                                         center=True)
 
-    # pick random seeds for volume distribution
+    # pick random seeds for fiber population distribution
     seeds_0 = seeds[np.random.rand(seeds.shape[0]) < psi, :]
     seeds_1 = seeds[np.random.rand(seeds.shape[0]) < (1 - psi), :]
     rnd_radii_0 = RADIUS_LOGMEAN * np.random.lognormal(0, 0.1, seeds_0.shape[0])
@@ -106,7 +108,7 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
         fastpli.model.sandbox.build.cuboid(
             p=-0.5 * np.array([LENGTH, LENGTH, LENGTH]),
             q=0.5 * np.array([LENGTH, LENGTH, LENGTH]),
-            phi=np.deg2rad(dphi),
+            phi=np.deg2rad(omega),
             theta=np.deg2rad(90),
             seeds=seeds_1 + RADIUS_LOGMEAN,
             radii=rnd_radii_1)
@@ -127,7 +129,7 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
     fiber_bundles = solver.apply_boundary_conditions(100)
     logger.info(f"rnd displacement: {solver.num_obj}/{solver.num_col_obj}")
 
-    # if comm.Get_rank() == 0:
+    # if comm.Get_size() == 1:
     #     solver.draw_scene()
 
     # Save Data
@@ -135,8 +137,8 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
     with h5py.File(file_pref + '.init.h5', 'w-') as h5f:
         solver.save_h5(h5f, script=open(os.path.abspath(__file__), 'r').read())
         h5f['/'].attrs['psi'] = psi
-        h5f['/'].attrs['dphi'] = dphi
-    fastpli.io.fiber_bundles.save(file_pref + '.init.dat', solver.fiber_bundles)
+        h5f['/'].attrs['omega'] = omega
+    # fastpli.io.fiber_bundles.save(file_pref + '.init.dat', solver.fiber_bundles)
 
     # Run Solver
     logger.info(f"run solver")
@@ -154,11 +156,10 @@ for dphi, psi in tqdm(PARAMETER[comm.Get_rank()::comm.Get_size()]):
             break
 
     logger.info(f"solved: {i}, {solver.num_obj}/{solver.num_col_obj}")
-
     logger.debug(f"save solved")
     with h5py.File(file_pref + '.solved.h5', 'w-') as h5f:
         solver.save_h5(h5f, script=open(os.path.abspath(__file__), 'r').read())
         h5f['/'].attrs['psi'] = psi
-        h5f['/'].attrs['dphi'] = dphi
+        h5f['/'].attrs['omega'] = omega
     fastpli.io.fiber_bundles.save(file_pref + '.solved.dat',
                                   solver.fiber_bundles)
