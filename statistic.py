97ddfffd44499eae45fc32a488f3484be2d35858
fmatuschke <f.matuschke@fz-juelich.de>
2020-02-25 14:56:15 +0100

    plot of vonmises.pdf

---
+++ statistic.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def _remap_direction(phi):
+    phi = phi % np.pi
+    phi[phi < 0] += np.pi
+    return phi
+
+
+def _remap_orientation(phi, theta):
+    phi = phi % (2 * np.pi)
+    theta = theta % np.pi
+
+    phi[phi < 0] += 2 * np.pi
+
+    phi[theta < 0] += np.pi
+    theta = np.abs(theta)
+
+    phi[theta > .5 * np.pi] += np.pi
+    theta[theta > .5 * np.pi] = np.pi - theta[theta >= .5 * np.pi]
+
+    phi = phi % (2 * np.pi)
+
+    if np.any(phi < 0) or np.any(phi >= 2 * np.pi) or np.any(
+            theta < 0) or np.any(theta > 0.5 * np.pi):
+        raise ValueError
+
+    return phi, theta
+
+
+def direction_histogram(data, N=2 * 36, ax=None):
+    data = np.array(data, copy=False)
+    data = _remap_direction(data.flatten())
+    data = np.vstack([data, data + np.pi]).flatten()
+    hist, bins = np.histogram(data,
+                              np.linspace(0, 2 * np.pi, N + 1, True),
+                              density=True)
+
+    # data = np.rad2deg(data)
+    # hist, bins = np.histogram(data,
+    #                           np.linspace(0, 360, N + 1, True),
+    #                           density=True)
+
+    if ax:
+        colors = plt.cm.viridis(hist / np.amax(hist))
+        ax.bar(np.deg2rad(bins[:-1]),
+               hist,
+               width=2 * np.pi / N,
+               bottom=0,
+               color=colors)
+        # ax.plot(np.deg2rad(bins[:-1]), hist)
+
+    return (bins[:-1] + bins[1:]) / 2, hist
+
+
+def orientation_histogram(phi, theta, Nphi=4 * 36, Ntheta=2 * 18, ax=None):
+    phi = np.array(phi, copy=False)
+    theta = np.array(theta, copy=False)
+    phi, theta = _remap_orientation(phi.flatten(), theta.flatten())
+    phi = np.rad2deg(phi)
+    theta = np.rad2deg(theta)
+
+    #calculate histogram
+    H, xedges, yedges = np.histogram2d(phi,
+                                       theta, [
+                                           np.linspace(0, 360, Nphi + 1, True),
+                                           np.linspace(0, 90, Ntheta + 1, True)
+                                       ],
+                                       density=True)
+
+    H = H.T
+
+    if ax:
+        X, Y = np.meshgrid(xedges, yedges)
+        ax.pcolormesh(np.deg2rad(X), np.deg2rad(Y), H, cmap="viridis")
+
+    X, Y = np.meshgrid(xedges + (xedges[1] - xedges[0]) / 2,
+                       yedges + (yedges[1] - yedges[0]) / 2)
+
+    H = np.concatenate([H, np.atleast_2d(H[0, :])], axis=0)
+    H = np.concatenate([H, np.atleast_2d(H[:, 0]).T], axis=1)
+
+    # X, Y = np.meshgrid((xedges[:-1] + xedges[1:]) / 2,
+    #                    (yedges[:-1] + yedges[1:]) / 2)
+
+    return H, X, Y
+
+
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = plt.subplot(211, polar=True)
+    direction = np.random.normal(0, 0.2, 1000)
+    direction_histogram(direction, 2 * 36, ax=ax)
+    ax.set_yticklabels([])
+
+    ax = plt.subplot(212, polar=True)
+    inclination = np.random.normal(0.2, 0.2, 1000)
+    h, x, y = orientation_histogram(direction,
+                                    np.pi / 2 - inclination,
+                                    18,
+                                    6,
+                                    ax=ax)
+    ax.set_yticklabels([])
+
+    with open('ohist.dat', 'w') as f:
+        for i, j, k in zip(x, y, h):
+            for a, b, c in zip(i, j, k):
+                f.write(f"{a} {b} {c}\n")
+            f.write("\n")
+
+    plt.show()
--- voxel_size.py
+++ voxel_size.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from tqdm import tqdm
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
 
-NUM_THREADS = 16
+NUM_THREADS = 4
 
 # reproducability
 np.random.seed(42)
@@ -22,153 +24,305 @@ FILE_BASE = os.path.basename(FILE_NAME)
 FILE_NAME = os.path.splitext(FILE_BASE)[0]
 FILE_NAME = os.path.join('output/', FILE_NAME)
 
-if False:
+LENGTH = 5
+VOXEL_SIZES = [
+    0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.14, 0.16,
+    0.18, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5
+]
 
-    import fastpli.simulation
-    import fastpli.analysis
-    import fastpli.objects
-    import fastpli.tools
-    import fastpli.io
+for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
 
-    # PARAMETER
-    LENGTH = 0.1
-    THICKNESS = 60
-    FIBER_INCLINATION = np.linspace(0, 90, 10, True)
-    # VOXEL_SIZES = [0.0625 * 2**i for i in range(7)]  # 1/2**4
-    # VOXEL_SIZES = [
-    #     0.0625 * i for i in
-    #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 60, 120, 240, 480, 960]
-    # ]  # 1/2**4
+    INPUT_FILE = f'input/cube_2pop_psi_0.5_omega_{omega}.0.solved.h5'
 
-    VOXEL_SIZES = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1]
+    OUTPUT_NAME = FILE_NAME + '_' + os.path.basename(
+        INPUT_FILE) + '_vref_' + str(VOXEL_SIZES[0]) + '_size_' + str(LENGTH)
 
-    fiber_bundles = fastpli.io.fiber_bundles.load(
-        'input/cube_2pop_psi_0.5_omega_0.0.solved.h5', '/')
+    if not os.path.isfile(OUTPUT_NAME + '.h5'):
+        print("simulate data")
 
-    os.makedirs('output', exist_ok=True)
+        import fastpli.simulation
+        import fastpli.analysis
+        import fastpli.objects
+        import fastpli.tools
+        import fastpli.io
 
-    with h5py.File(FILE_NAME + '.h5', 'w-') as h5f:
-        with open(os.path.abspath(__file__), 'r') as script:
-            h5f.attrs['script'] = script.read()
+        # PARAMETER
+        THICKNESS = 60
+        FIBER_INCLINATION = np.linspace(0, 90, 10, True)
 
-        for voxel_size in tqdm(VOXEL_SIZES):
-            for dn, model in [(-0.001, 'p'), (0.002, 'r')]:
-                dset = h5f.create_group(str(voxel_size) + '/' + model)
+        fiber_bundles = fastpli.io.fiber_bundles.load(INPUT_FILE, '/')
 
-                # Setup Simpli
-                simpli = fastpli.simulation.Simpli()
-                simpli.omp_num_threads = NUM_THREADS
-                simpli.voxel_size = voxel_size
-                simpli.resolution = voxel_size
-                simpli.filter_rotations = np.deg2rad([0, 30, 60, 90, 120, 150])
-                simpli.interpolate = True
-                simpli.untilt_sensor_view = True
-                simpli.wavelength = 525  # in nm
+        os.makedirs('output', exist_ok=True)
 
-                simpli.set_voi(-0.5 * np.array([LENGTH, LENGTH, THICKNESS]),
-                               0.5 * np.array([LENGTH, LENGTH, THICKNESS]))
+        with h5py.File(OUTPUT_NAME + '.h5', 'w-') as h5f:
+            with open(os.path.abspath(__file__), 'r') as script:
+                h5f.attrs['script'] = script.read()
 
-                # simpli.tilts = np.deg2rad(
-                #     np.array([(0, 0), (5.5, 0), (5.5, 90), (5.5, 180),
-                #               (5.5, 270)]))
-                simpli.tilts = np.deg2rad(np.array([(0, 0)]))
-                simpli.add_crop_tilt_halo()
+            for voxel_size in tqdm(VOXEL_SIZES):
+                for dn, model in [(-0.001, 'p'), (0.002, 'r')]:
+                    dset = h5f.create_group(str(voxel_size) + '/' + model)
 
-                # print(simpli.dim)
+                    # Setup Simpli
+                    simpli = fastpli.simulation.Simpli()
+                    simpli.omp_num_threads = NUM_THREADS
+                    simpli.voxel_size = voxel_size
+                    simpli.resolution = voxel_size
+                    simpli.filter_rotations = np.deg2rad(
+                        [0, 30, 60, 90, 120, 150])
+                    simpli.interpolate = True
+                    simpli.untilt_sensor_view = True
+                    simpli.wavelength = 525  # in nm
 
-                print(str(round(simpli.memory_usage(), 2)) + 'MB')
-                time.sleep(4.2)
+                    simpli.set_voi(-0.5 * np.array([LENGTH, LENGTH, THICKNESS]),
+                                   0.5 * np.array([LENGTH, LENGTH, THICKNESS]))
 
-                simpli.fiber_bundles = fiber_bundles
-                simpli.fiber_bundles_properties = [[(0.75, 0, 0, 'b'),
-                                                    (1.0, dn, 0, model)]
-                                                  ] * len(fiber_bundles)
-                label_field, vector_field, tissue_properties = simpli.run_tissue_pipeline(
-                    h5f=dset, save=["label_field"])
+                    # simpli.tilts = np.deg2rad(
+                    #     np.array([(0, 0), (5.5, 0), (5.5, 90), (5.5, 180),
+                    #               (5.5, 270)]))
+                    simpli.tilts = np.deg2rad(np.array([(0, 0)]))
+                    simpli.add_crop_tilt_halo()
 
-                # Simulate PLI Measurement
-                simpli.light_intensity = 1  # a.u.
-                simpli.save_parameter_h5(h5f=dset)
-                for t, tilt in enumerate(simpli._tilts):
-                    theta, phi = tilt[0], tilt[1]
-                    images = simpli.run_simulation(label_field, vector_field,
-                                                   tissue_properties, theta,
-                                                   phi)
+                    # print(simpli.dim)
+                    if simpli.memory_usage() > 1e3:
+                        print(str(round(simpli.memory_usage(), 2)) + 'MB')
+                        time.sleep(4.2)
 
-                    images = simpli.rm_crop_tilt_halo(images)
+                    simpli.fiber_bundles = fiber_bundles
+                    simpli.fiber_bundles_properties = [[(0.75, 0, 0, 'b'),
+                                                        (1.0, dn, 0, model)]
+                                                      ] * len(fiber_bundles)
+                    label_field, vector_field, tissue_properties = simpli.run_tissue_pipeline(
+                        h5f=dset, save=["label_field"])
 
-                    dset['simulation/data/' + str(t)] = images
-                    dset['simulation/data/' + str(t)].attrs['theta'] = theta
-                    dset['simulation/data/' + str(t)].attrs['phi'] = phi
+                    # Simulate PLI Measurement
+                    simpli.light_intensity = 1  # a.u.
+                    simpli.save_parameter_h5(h5f=dset)
+                    for t, tilt in enumerate(simpli._tilts):
+                        theta, phi = tilt[0], tilt[1]
+                        images = simpli.run_simulation(label_field,
+                                                       vector_field,
+                                                       tissue_properties, theta,
+                                                       phi)
 
-                del label_field
-                del vector_field
-                del simpli
+                        images = simpli.rm_crop_tilt_halo(images)
 
-if True:
-    import fastpli.simulation.optic
-    with h5py.File(FILE_NAME + '.h5', 'r') as h5f:
-        voxel_size_ref = str(min([float(i) for i in h5f]))
-        print(voxel_size_ref)
-        data_ref = {}
-        data_ref['r'] = h5f[voxel_size_ref + '/r/simulation/data/0'][:]
-        data_ref['p'] = h5f[voxel_size_ref + '/p/simulation/data/0'][:]
+                        dset['simulation/data/' + str(t)] = images
+                        dset['simulation/data/' + str(t)].attrs['theta'] = theta
+                        dset['simulation/data/' + str(t)].attrs['phi'] = phi
 
-        df = pd.DataFrame()
-        for voxel_size in h5f:
-            for model in h5f[voxel_size]:
-                dset = h5f[voxel_size + '/' + model]
+                    del label_field
+                    del vector_field
+                    del simpli
 
-                data = dset['simulation/data/0'][:]
+for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
+    INPUT_FILE = f'input/cube_2pop_psi_0.5_omega_{omega}.0.solved.h5'
 
-                data_ref_optic = np.empty_like(data)
-                scale = float(voxel_size_ref) / float(voxel_size)
+    OUTPUT_NAME = FILE_NAME + '_' + os.path.basename(
+        INPUT_FILE) + '_vref_' + str(VOXEL_SIZES[0]) + '_size_' + str(LENGTH)
+    if not os.path.isfile(OUTPUT_NAME + '.epa.pkl'):
+        print("analyse data")
+        import fastpli.simulation.optic
+        import fastpli.analysis
+        with h5py.File(OUTPUT_NAME + '.h5', 'r') as h5f:
+            voxel_size_ref = str(min([float(i) for i in h5f]))
+            print(voxel_size_ref)
+            data_ref = {}
+            data_ref['r'] = h5f[voxel_size_ref + '/r/simulation/data/0'][:]
+            data_ref['p'] = h5f[voxel_size_ref + '/p/simulation/data/0'][:]
 
-                if scale != 1:
-                    for i in range(data_ref[model].shape[-1]):
-                        data_ref_optic[:, :,
-                                       i] = fastpli.simulation.optic.resample(
-                                           data_ref[model][:, :, i],
-                                           float(voxel_size_ref) /
-                                           float(voxel_size))
-                else:
-                    data_ref_optic = data_ref[model].copy()
+            df_data = pd.DataFrame()
+            df_epa = pd.DataFrame()
+            for voxel_size in h5f:
+                for model in h5f[voxel_size]:
+                    dset = h5f[voxel_size + '/' + model]
 
-                data_ref_optic = np.array(data_ref_optic)
+                    data = dset['simulation/data/0'][:]
 
-                df_ = pd.DataFrame({
-                    'voxel_size': [float(voxel_size)] * data.size,
-                    'model': [model] * data.size,
-                    'data':
-                        data.flatten(),
-                    'data_ref':
-                        data_ref_optic.flatten(),
-                    'diff':
-                        np.abs(data - data_ref_optic).flatten(),
-                    'dev':
-                        np.abs(data - data_ref_optic).flatten() /
-                        data_ref_optic.flatten(),
-                })
+                    data_ref_optic = np.empty_like(data)
+                    scale = float(voxel_size_ref) / float(voxel_size)
 
-                df = df.append(df_)
+                    if scale != 1:
+                        for i in range(data_ref[model].shape[-1]):
+                            data_ref_optic[:, :,
+                                           i] = fastpli.simulation.optic.resample(
+                                               data_ref[model][:, :, i],
+                                               float(voxel_size_ref) /
+                                               float(voxel_size))
+                    else:
+                        data_ref_optic = data_ref[model].copy()
 
-        df.to_pickle(FILE_NAME + '.pkl')
+                    data_ref_optic = np.array(data_ref_optic)
 
-df = pd.read_pickle(FILE_NAME + '.pkl')
-# df = df.where(df['voxel_size'] < 0.1)
+                    df1 = pd.DataFrame({
+                        'voxel_size': [float(voxel_size)] * data.size,
+                        'model': [model] * data.size,
+                        'data':
+                            data.flatten(),
+                        'data_ref':
+                            data_ref_optic.flatten(),
+                        'diff':
+                            np.abs(data - data_ref_optic).flatten(),
+                        'dev':
+                            np.abs(data - data_ref_optic).flatten() /
+                            data_ref_optic.flatten(),
+                    })
 
-# print(df['voxel_size'])
-# sys.exit()
-# print(df['voxel_size'])
+                    trans, dirc, ret = fastpli.analysis.epa.epa(data)
 
-df1 = df[['model', 'dev', 'voxel_size']]
-# df1 = df1.loc[df1['model'] == 'r']
+                    dirc[dirc > np.pi / 2] -= np.pi
 
-fig, axes = plt.subplots(nrows=2, ncols=1)
-# df1.boxplot()
-df1[df1['model'] == 'p'].boxplot(ax=axes[0], by=['voxel_size'], column=['dev'])
-df1[df1['model'] == 'r'].boxplot(ax=axes[1], by=['voxel_size'], column=['dev'])
-plt.show()
+                    df2 = pd.DataFrame({
+                        'voxel_size': [float(voxel_size)] * trans.size,
+                        'model': [model] * trans.size,
+                        'transmittance': trans.flatten(),
+                        'direction': dirc.flatten(),
+                        'retardation': ret.flatten()
+                    })
 
-# # import tikzplotlib
-# # tikzplotlib.save("test.tex")
+                    df_data = df_data.append(df1)
+                    df_epa = df_epa.append(df2)
+
+            df_data.to_pickle(OUTPUT_NAME + '.data.pkl')
+            df_epa.to_pickle(OUTPUT_NAME + '.epa.pkl')
+
+import seaborn as sns
+import statistic
+import scipy.stats
+import scipy.optimize
+import astropy.stats
+
+df_circ = pd.DataFrame()
+
+# fig, axs = plt.subplots(nrows=1, ncols=1, subplot_kw={'projection': 'polar'})
+# axes = axs.reshape(-1)
+
+# fig, axs = plt.subplots(nrows=len(VOXEL_SIZES),
+#                         ncols=10,
+#                         subplot_kw={'projection': 'polar'})
+# axes = axs.reshape(-1)
+
+fig, ax = plt.subplots(1, 1)
+
+for omega in list(range(0, 100, 10))[::-1][comm.Get_rank()::comm.Get_size()]:
+    INPUT_FILE = f'input/cube_2pop_psi_0.5_omega_{omega}.0.solved.h5'
+
+    OUTPUT_NAME = FILE_NAME + '_' + os.path.basename(
+        INPUT_FILE) + '_vref_' + str(VOXEL_SIZES[0]) + '_size_' + str(LENGTH)
+    df = pd.read_pickle(OUTPUT_NAME + '.epa.pkl')
+    # df[df['model'] == 'p'].boxplot(
+    #     by=['voxel_size'], column=['transmittance', 'direction', 'retardation'])
+    # df[df['model'] == 'r'].boxplot(
+    #     by=['voxel_size'], column=['transmittance', 'direction', 'retardation'])
+
+    # n = int(np.ceil(np.sqrt(df['voxel_size'].nunique())))
+    # fig, axs = plt.subplots(nrows=n, ncols=n, subplot_kw={'projection': 'polar'})
+    # axes = axs.reshape(-1)
+
+    # for i, (name, group) in enumerate(df[df['model'] == 'p'].groupby('voxel_size')):
+    #     direction_plot(group['direction'].to_numpy(), ax=axes[i])
+
+    # plt.show()
+
+    # fig, axs = plt.subplots()
+
+    for m in ['p']:
+
+        df_dir = pd.DataFrame()
+        for i, (name,
+                group) in enumerate(df[df['model'] == m].groupby('voxel_size')):
+            data = group['direction'].to_numpy()
+
+            ax.clear()
+
+            # fig, axs = plt.subplots(nrows=1,
+            #                         ncols=1,
+            #                         subplot_kw={'projection': 'polar'})
+
+            x, y = statistic.direction_histogram(data, N=36, ax=None)
+            # statistic.direction_histogram(data, N=36, ax=axs)
+
+            mu, kappa = astropy.stats.vonmisesmle(data * 2)
+
+            muu, kappaa, pp = scipy.stats.vonmises.fit(data * 2,
+                                                       kappa,
+                                                       loc=mu,
+                                                       scale=1)
+
+            (muuu, kappaaa), _ = scipy.optimize.curve_fit(
+                lambda x, m, k: scipy.stats.vonmises.pdf(x - m, k),
+                x * 2,
+                y,
+                p0=(mu, kappa),
+                bounds=(0, [2 * np.pi, 10]),
+                # method='trf'
+            )
+
+            xx = np.linspace(0, 4 * np.pi, 100)
+
+            plt.plot(x, y)
+            plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - mu, kappa))
+            plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - muu, kappaa))
+            plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - muuu, kappaaa))
+
+            print(kappa, mu)
+            print(kappaa, muu, pp)
+            print(kappaaa, muuu)
+            print()
+
+            plt.ion()
+            plt.show()
+            plt.pause(0.001)
+
+            if i == 0:
+                df_ = pd.DataFrame({'direction': x, name: y})
+            else:
+                df_ = pd.DataFrame({name: y})
+
+            df_dir = pd.concat([df_dir, df_], axis=1)
+
+            # sys.exit()
+
+        #     df_circ = df_circ.append(
+        #         {
+        #             "voxel_size":
+        #                 float(name),
+        #             "model":
+        #                 m,
+        #             "omega":
+        #                 float(omega),
+        #             "diff_mean":
+        #                 scipy.stats.circmean(group['direction'].to_numpy()),
+        #             "diff_std":
+        #                 scipy.stats.circstd(group['direction'].to_numpy())
+        #         },
+        #         ignore_index=True)
+
+        df_dir.to_csv(OUTPUT_NAME + f'.direction.voxel_size.{m}' + '.csv')
+
+# plt.show()
+
+# df_circ.to_csv(OUTPUT_NAME + '.circ' + '.csv')
+
+# fig, ax = plt.subplots()
+# for i, (name,
+#         group) in enumerate(df_circ[df_circ['model'] == m].groupby('omega')):
+#     group.plot(x='voxel_size', y='diff_mean', yerr='diff_std', ax=ax)
+# plt.show()
+
+# break
+
+# plt.show()
+
+# import tikzplotlib
+# tikzplotlib.save("test.tex")
+
+# with open('test.tex', 'r') as file:
+#     filedata = file.read()
+
+# filedata = filedata.replace(
+#     r'\begin{document}',
+#     r'\usepackage{siunitx} \usepgfplotslibrary{polar} \begin{document}')
+# filedata = filedata.replace('{axis}', '{polaraxis}')
+
+# with open('test.tex', 'w') as file:
+#     file.write(filedata)
