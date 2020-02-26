c61df083f296c4c51ac5729c7e09dccc4587229d
fmatuschke <f.matuschke@fz-juelich.de>
2020-02-26 10:25:43 +0100

    trans, dir, ret diff plot

--- voxel_size.py
+++ voxel_size.py
@@ -114,7 +114,7 @@ for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
                     del vector_field
                     del simpli
 
-for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
+for omega in list(range(0, 100, 10)):
     INPUT_FILE = f'input/cube_2pop_psi_0.5_omega_{omega}.0.solved.h5'
 
     OUTPUT_NAME = FILE_NAME + '_' + os.path.basename(
@@ -156,18 +156,18 @@ for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
                     df1 = pd.DataFrame({
                         'voxel_size': [float(voxel_size)] * data.size,
                         'model': [model] * data.size,
-                        'data':
-                            data.flatten(),
-                        'data_ref':
-                            data_ref_optic.flatten(),
-                        'diff':
-                            np.abs(data - data_ref_optic).flatten(),
-                        'dev':
-                            np.abs(data - data_ref_optic).flatten() /
-                            data_ref_optic.flatten(),
+                        'data': data.flatten(),
+                        'data_ref': data_ref_optic.flatten(),
+                        # 'diff':
+                        #     np.abs(data - data_ref_optic).flatten(),
+                        # 'dev':
+                        #     np.abs(data - data_ref_optic).flatten() /
+                        #     data_ref_optic.flatten(),
                     })
 
                     trans, dirc, ret = fastpli.analysis.epa.epa(data)
+                    ref_trans, ref_dirc, ref_ret = fastpli.analysis.epa.epa(
+                        data_ref_optic)
 
                     dirc[dirc > np.pi / 2] -= np.pi
 
@@ -176,7 +176,10 @@ for omega in list(range(0, 100, 10))[comm.Get_rank()::comm.Get_size()]:
                         'model': [model] * trans.size,
                         'transmittance': trans.flatten(),
                         'direction': dirc.flatten(),
-                        'retardation': ret.flatten()
+                        'retardation': ret.flatten(),
+                        'opt_transmittance': ref_trans.flatten(),
+                        'opt_direction': ref_dirc.flatten(),
+                        'opt_retardation': ref_ret.flatten()
                     })
 
                     df_data = df_data.append(df1)
@@ -191,190 +194,61 @@ import scipy.stats
 import scipy.optimize
 import astropy.stats
 
-df_circ = pd.DataFrame()
+df_analyse = pd.DataFrame()
 
-# fig, axs = plt.subplots(nrows=1, ncols=1, subplot_kw={'projection': 'polar'})
-# axes = axs.reshape(-1)
-
-# fig, axs = plt.subplots(nrows=len(VOXEL_SIZES),
-#                         ncols=10,
-#                         subplot_kw={'projection': 'polar'})
-# axes = axs.reshape(-1)
-
-# fig, ax = plt.subplots(1, 1)
-
-for omega in list(range(0, 100, 10))[::-1][comm.Get_rank()::comm.Get_size()]:
+for omega in list(range(0, 100, 10)):
     INPUT_FILE = f'input/cube_2pop_psi_0.5_omega_{omega}.0.solved.h5'
 
     OUTPUT_NAME = FILE_NAME + '_' + os.path.basename(
         INPUT_FILE) + '_vref_' + str(VOXEL_SIZES[0]) + '_size_' + str(LENGTH)
     df = pd.read_pickle(OUTPUT_NAME + '.epa.pkl')
-    # df[df['model'] == 'p'].boxplot(
-    #     by=['voxel_size'], column=['transmittance', 'direction', 'retardation'])
-    # df[df['model'] == 'r'].boxplot(
-    #     by=['voxel_size'], column=['transmittance', 'direction', 'retardation'])
 
-    # n = int(np.ceil(np.sqrt(df['voxel_size'].nunique())))
-    # fig, axs = plt.subplots(nrows=n, ncols=n, subplot_kw={'projection': 'polar'})
-    # axes = axs.reshape(-1)
-
-    # for i, (name, group) in enumerate(df[df['model'] == 'p'].groupby('voxel_size')):
-    #     direction_plot(group['direction'].to_numpy(), ax=axes[i])
-
-    # plt.show()
-
-    # fig, axs = plt.subplots()
-
-    df_dir = pd.DataFrame()
-
-    data_ref = df[np.logical_and(
-        df['model'] == 'r',
-        df['voxel_size'] == VOXEL_SIZES[0])]['direction'].to_numpy()
-    x_ref, y_ref = statistic.direction_histogram(data_ref, N=2 * 36, ax=None)
+    df_ref = df[(df['model'] == 'r') & (df['voxel_size'] == VOXEL_SIZES[0])]
 
     for m in ['p', 'r']:
-
-        # df_dir = pd.DataFrame()
-
-        # data_ref = df[np.logical_and(
-        #     df['model'] == m,
-        #     df['voxel_size'] == VOXEL_SIZES[0])]['direction'].to_numpy()
-        # x_ref, y_ref = statistic.direction_histogram(data_ref,
-        #                                              N=2 * 36,
-        #                                              ax=None)
-
         for i, (name,
                 group) in enumerate(df[df['model'] == m].groupby('voxel_size')):
-            data = group['direction'].to_numpy()
 
-            # ax.clear()
-
-            # fig, axs = plt.subplots(nrows=1,
-            #                         ncols=1,
-            #                         subplot_kw={'projection': 'polar'})
-
-            x, y = statistic.direction_histogram(data, N=2 * 36, ax=None)
-            # statistic.direction_histogram(data, N=36, ax=axs)
-
-            # mu, kappa = astropy.stats.vonmisesmle(data * 2)
-
-            # muu, kappaa, pp = scipy.stats.vonmises.fit(data * 2,
-            #                                            kappa,
-            #                                            loc=mu,
-            #                                            scale=1)
-
-            # (muuu, kappaaa), _ = scipy.optimize.curve_fit(
-            #     lambda x, m, k: scipy.stats.vonmises.pdf(x - m, k),
-            #     x * 2,
-            #     y,
-            #     p0=(mu, kappa),
-            #     bounds=(0, [2 * np.pi, 10]),
-            #     # method='trf'
-            # )
-
-            # xx = np.linspace(0, 4 * np.pi, 100)
-
-            # plt.plot(x, y)
-            # plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - mu, kappa))
-            # plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - muu, kappaa))
-            # plt.plot(xx / 2, scipy.stats.vonmises.pdf(xx - muuu, kappaaa))
-
-            # print(kappa, mu)
-            # print(kappaa, muu, pp)
-            # print(kappaaa, muuu)
-            # print()
-
-            # plt.ion()
-            # plt.show()
-            # plt.pause(0.001)
-
-            if i == 0:
-                df_ = pd.DataFrame({'direction': x, name: y})
-            else:
-                df_ = pd.DataFrame({name: y})
-
-            df_dir = pd.concat([df_dir, df_], axis=1)
-
-            # sys.exit()
-
-            df_circ = df_circ.append(
+            df_analyse = df_analyse.append(
                 {
-                    "voxel_size": float(name),
-                    "model": m,
-                    "omega": float(omega),
-                    "delta": np.mean((y_ref - y)**2)
+                    "voxel_size":
+                        float(name),
+                    "model":
+                        m,
+                    "omega":
+                        float(omega),
+                    "mean_diff_dir":
+                        np.mean(
+                            np.abs(group['opt_direction'].to_numpy() -
+                                   group['direction'].to_numpy())),
+                    "mean_diff_ret":
+                        np.mean(
+                            np.abs(group['opt_retardation'].to_numpy() -
+                                   group['retardation'].to_numpy())),
+                    "mean_diff_rel_ret":
+                        np.mean(
+                            np.abs(group['opt_retardation'].to_numpy() -
+                                   group['retardation'].to_numpy()) /
+                            group['opt_retardation'].to_numpy()),
+                    "mean_diff_trans":
+                        np.mean(
+                            np.abs(group['opt_transmittance'].to_numpy() -
+                                   group['transmittance'].to_numpy()))
                 },
                 ignore_index=True)
 
-            # df_circ = df_circ.append(
-            #     {
-            #         "voxel_size":
-            #             float(name),
-            #         "model":
-            #             m,
-            #         "omega":
-            #             float(omega),
-            #         "diff_mean":
-            #             scipy.stats.circmean(group['direction'].to_numpy()),
-            #         "diff_std":
-            #             scipy.stats.circstd(group['direction'].to_numpy())
-            #     },
-            #     ignore_index=True)
-
-        # df_dir.to_csv(OUTPUT_NAME + f'.direction.voxel_size.{m}' + '.csv')
-
-# plt.show()
-
-# df_circ.to_csv(OUTPUT_NAME + '.circ' + '.csv')
-
-# fig, ax = plt.subplots()
-# for i, (name,
-#         group) in enumerate(df_circ[df_circ['model'] == m].groupby('omega')):
-#     group.plot(x='voxel_size', y='diff_mean', yerr='diff_std', ax=ax)
-# plt.show()
-
-# break
-
-# plt.show()
-
-# import tikzplotlib
-# tikzplotlib.save("test.tex")
-
-# with open('test.tex', 'r') as file:
-#     filedata = file.read()
-
-# filedata = filedata.replace(
-#     r'\begin{document}',
-#     r'\usepackage{siunitx} \usepgfplotslibrary{polar} \begin{document}')
-# filedata = filedata.replace('{axis}', '{polaraxis}')
-
-# with open('test.tex', 'w') as file:
-#     file.write(filedata)
-
-# df_circ.groupby(['voxel_size', 'model']).count()['delta'].unstack().plot()
-
-# print(df_circ.groupby(['voxel_size', 'model']))
-# print(df_circ.groupby(['voxel_size', 'model']).count())
-# print(df_circ.groupby(['voxel_size', 'model']).count()['delta'])
-# print(df_circ.groupby(['voxel_size', 'model']).count()['delta'].unstack())
-# plt.show()
-
-# df_circ.pivot(index='voxel_size', columns='model', values='delta')
-
-fig, ax = plt.subplots()
-# fig, axs = plt.subplots(nrows=4, ncols=3)
-# ax = axs.reshape(-1)
-
-i = 0
-
-for key, grp in df_circ.groupby(['omega']):
-    for k, g in grp.groupby(['model']):
-        # print(g)
-        g.plot(ax=ax,
-               kind='line',
-               x='voxel_size',
-               y='delta',
-               label=str(key) + '_' + k)
-    i += 1
+for mod in ['trans', 'dir', 'ret', 'rel_ret']:
+    fig, ax = plt.subplots(2, 1)
+    fig.suptitle(mod)
+    for i, (k, g) in enumerate(df_analyse.groupby(['model'])):
+        ax[i].set_title(k)
+        for key, grp in g.groupby(['omega']):
+            # print(g)
+            grp.plot(ax=ax[i],
+                     kind='line',
+                     x='voxel_size',
+                     y='mean_diff_' + mod,
+                     marker='d',
+                     label=str(key) + '_' + k)
 
 plt.show()
