1a51fd56a7c82c4e5cf82c013903f5ae551d2a2e
fmatuschke <f.matuschke@fz-juelich.de>
2020-04-17 14:19:15 +0200

    ADD sync_core.sh

---
+++ sync_core.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "syncing core"
+
+if [ -z "$SSH_AGENT_PID" ]; then
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
+fi
+
+for d in [0-9]_*/ ; do
+
+	if [ d == "0_core" ]; then
+		continue
+	fi
+
+	(
+	echo "$d"
+	cd $d
+	if [ ! $(git rev-parse --show-superproject-working-tree) ] ; then
+		echo "$d no git repository"
+		continue
+	fi
+
+	if [ -n "$(git status -s)" ]; then
+		echo "$d modified"
+		continue
+	fi
+
+	rsync -au --exclude='.git' ../0_core/ .
+
+	if [ -n "$(git status -s)" ]; then
+		git add .
+
+		git commit -m "SYNC CORE"
+		git push
+		echo ""
+	fi
+	)
+
+done
+
+echo "... done"
