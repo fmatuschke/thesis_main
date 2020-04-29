c4659a20c2891183b2b8b8382a85bebfb0384bc0
fmatuschke <f.matuschke@fz-juelich.de>
2020-04-29 15:33:46 +0200

    RFC jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -2,25 +2,29 @@
 
 set -euo pipefail
 
+if [ -f "/usr/bin/python3.8" ]; then
+   python3='/usr/bin/python3.8'
+else
+   python3=python3
+fi
+
 if [ ! -d "env-$(hostname)" ]; then
-   python3 -m venv env-$(hostname)
+   $python3 -m venv env-$(hostname)
 fi
 
-if [ ! -f "env-$(hostname)/bin/pip3" ]; then
-   echo "venv comprimised, rm and reinstall"
-   rm -rf env-$(hostname)
-   python3 -m venv env-$(hostname)
+source env-$(hostname)/bin/activate
+
+if ! pip3 freeze | grep jupyterlab= -q; then
+   pip3 install jupyterlab
+   pip3 install jupyterlab-git
+   pip3 install jupyterthemes
+   pip3 install jupyter_contrib_nbextensions
+   jupyter contrib nbextension install --user
+   jt -t onedork -cellw 99% -T -nf ptsans -lineh 125
 fi
 
-if ! env-$(hostname)/bin/pip3 freeze | grep jupyterlab= -q; then
-   env-$(hostname)/bin/pip3 install jupyterlab
-   env-$(hostname)/bin/pip3 install jupyterlab-git
-   env-$(hostname)/bin/pip3 install jupyterthemes
-   env-$(hostname)/bin/pip3 install jupyter_contrib_nbextensions
-   # jupyter contrib nbextension install --user
-   # jt -t onedork -cellw 99% -T -nf ptsans -lineh 125
-fi
+#pip3 install -r requirements.txt
 
-env-$(hostname)/bin/pip3 install -r requirements.txt
+
 # echo "install requirements.txt"
-# find . -maxdepth 2 -name "requirements.txt" -exec env-$(hostname)/bin/pip3 install -q -r {} \;
+# find . -maxdepth 2 -name "requirements.txt" -exec pip3 install -q -r {} \;
