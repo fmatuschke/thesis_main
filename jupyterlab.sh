b82252fc09ad931e061bc28debc5097282094ce4
fmatuschke <f.matuschke@fz-juelich.de>
2020-04-29 17:29:16 +0200

    UPDATE and jupyterlab.sh

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -2,14 +2,8 @@
 
 set -euo pipefail
 
-if [ -f "/usr/bin/python3.8" ]; then
-   python3='/usr/bin/python3.8'
-else
-   python3=python3
-fi
-
 if [ ! -d "env-$(hostname)" ]; then
-   $python3 -m venv env-$(hostname)
+   python3 -m venv env-$(hostname)
 fi
 
 source env-$(hostname)/bin/activate
@@ -23,8 +17,12 @@ if ! pip3 freeze | grep jupyterlab= -q; then
    jt -t onedork -cellw 99% -T -nf ptsans -lineh 125
 fi
 
-#pip3 install -r requirements.txt
-
+(
+cd fastpli
+make fastpli
+)
+pip3 install fastpli/.
+pip3 install -r requirements.txt
 
 # echo "install requirements.txt"
 # find . -maxdepth 2 -name "requirements.txt" -exec pip3 install -q -r {} \;
