3c3a5988f944aac2e7fd65dd66c2a5fe65fa2287
fmatuschke <f.matuschke@fz-juelich.de>
2020-04-30 13:31:56 +0200

    RFC jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -4,17 +4,20 @@ set -euo pipefail
 
 if [ ! -d "env-$(hostname)" ]; then
    python3 -m venv env-$(hostname)
+   pip3 install -U pip
 fi
 
 source env-$(hostname)/bin/activate
 
 if ! pip3 freeze | grep jupyterlab= -q; then
-   pip3 install jupyterlab
-   pip3 install jupyterlab-git
+   # pip3 install jupyterlab
+   # pip3 install jupyterlab-git
+   pip3 install jupyter
    pip3 install jupyterthemes
    pip3 install jupyter_contrib_nbextensions
    jupyter contrib nbextension install --user
    jt -t onedork -cellw 99% -T -nf ptsans -lineh 125
+   cp custom.css ~/.jupyter/custom/custom.css
 fi
 
 (
