dd3204c2ac51779212eb7585351fdab1ceb751b4
fmatuschke <f.matuschke@fz-juelich.de>
2020-05-28 07:40:10 +0200

    ADD angular_correlation_coefficient

---
+++ 0_core/helper/schilling.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" From Schilling et al. 10.1016/j.neuroimage.2017.10.046
+"""
+
+import numpy as np
+
+# def linear_sh_coeff_index(l, m):
+#     # num_coeff = ((l // 2) + 1) * (2 * (l // 2) + 1)
+#     return l**2 + m # not pli index
+
+
+def angular_correlation_coefficient(sh0, sh1):
+    return np.sum(np.product(sh0, sh1)) / np.sqrt(np.sum(np.product(sh0, sh0)),
+                                                  np.sum(np.product(sh1, sh1)))
+
+
+def jensen_shannon_divergence(sh0, sh1):
+    pass
