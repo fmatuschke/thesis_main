18ed03d7e77c5002e5a59d41fc68407936dd3c05
fmatuschke <f.matuschke@fz-juelich.de>
2020-05-28 11:59:05 +0200

    FIX schilling acc

--- 0_core/helper/schilling.py
+++ 0_core/helper/schilling.py
@@ -11,8 +11,8 @@ import numpy as np
 
 
 def angular_correlation_coefficient(sh0, sh1):
-    return np.sum(np.multiply(sh0, sh1)) / np.sqrt(np.sum(np.multiply(
-        sh0, sh0))) * np.sqrt(np.sum(np.multiply(sh1, sh1)))
+    return np.sum(np.multiply(sh0, sh1)) / (np.sqrt(
+        np.sum(np.multiply(sh0, sh0))) * np.sqrt(np.sum(np.multiply(sh1, sh1))))
 
 
 def jensen_shannon_divergence(sh0, sh1):
