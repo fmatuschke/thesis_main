f0b78c84a87a2efdb1d28d60a08975e42794aa8d
fmatuschke <f.matuschke@fz-juelich.de>
2020-05-29 12:25:38 +0200

    ADD cutting to model images

--- 2_cube_factory/pictures.py
+++ 2_cube_factory/pictures.py
@@ -1,7 +1,9 @@
 import fastpli.model.solver
+import fastpli.objects
 import fastpli.io
 
 import numpy as np
+import subprocess
 import argparse
 import sys
 import os
@@ -16,10 +18,24 @@ parser.add_argument("-i",
                     nargs='+',
                     required=True,
                     help="input path path of solver.")
+
+parser.add_argument("-o",
+                    "--output",
+                    type=str,
+                    required=True,
+                    help="output path path of solver.")
 args = parser.parse_args()
+
+os.makedirs(args.output, exist_ok=True)
 
 solver = fastpli.model.solver.Solver()
 for file in tqdm(args.input):
-    solver.fiber_bundles = fastpli.io.fiber_bundles.load(file)
+    fbs = fastpli.io.fiber_bundles.load(file)
+    file = os.path.join(args.output,
+                        os.path.splitext(os.path.basename(file))[0])
+    solver.fiber_bundles = fastpli.objects.fiber_bundles.Cut(
+        fbs, [[-30] * 3, [30] * 3])
     solver.draw_scene()
     solver.save_ppm(os.path.splitext(file)[0] + ".ppm")
+    subprocess.run(
+        ["convert", file + ".ppm", file + ".png", "&&", "rm", file + ".ppm"])
