5e3fd98b166dd021f3b7bc41517fd55d87831df0
fmatuschke <f.matuschke@fz-juelich.de>
2020-06-03 14:20:19 +0200

    ADD init vs solved

--- 2_cube_factory/simulation_analysis_init_1.py
+++ 2_cube_factory/simulation_analysis_init_1.py
@@ -26,31 +26,34 @@ import fastpli.analysis
 df = pd.read_pickle("output/analysis/cube_2pop_simulation_LAP_model_p_.pkl")
 f0_incs = df.f0_inc.unique()
 
-parameters = list(itertools.product(df.omega.unique(), df.psi.unique()))
+parameters = list(itertools.product(df.omega.unique(), df.psi.unique(), ["init", "solved"]))
 random.shuffle(parameters)
 parameters = parameters[comm.Get_rank()::comm.Get_size()]
 
-with tqdm(total=len(parameters) * len(f0_incs)) as pbar:
-    for omega, psi in parameters:
-        if os.path.isfile(
-                f"output/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.solved.pkl"
-        ):
-            continue
+with tqdm(total=len(parameters) * len(f0_incs) * 2) as pbar:
+        for omega, psi, state in parameters:
+            if os.path.isfile(
+                    f"output/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.{state}.pkl"
+            ):
+                pbar.update(len(f0_incs))
+                continue
+            if not os.path.isfile(
+                    f"../data/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.{state}.h5"
+            ):
+                pbar.update(len(f0_incs))
+                continue
 
-        fbs = fastpli.io.fiber_bundles.load(
-            f"../data/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.solved.h5"
-        )
+            fbs = fastpli.io.fiber_bundles.load(
+                f"../data/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.{state}.h5"
+            )
 
-        df_sub = df[(df.omega == omega) & (df.psi == psi)]
-        df_ = []
-        for f0_inc in f0_incs:
-            for f1_rot in df_sub[df_sub.f0_inc == f0_inc].f1_rot.unique():
-                rot_inc = fastpli.tools.rotation.y(-np.deg2rad(f0_inc))
-                rot_phi = fastpli.tools.rotation.x(np.deg2rad(f1_rot))
-                rot = np.dot(rot_inc, rot_phi)
-                fbs_ = fastpli.objects.fiber_bundles.Rotate(fbs, rot)
+            df_sub = df[(df.omega == omega) & (df.psi == psi)]
+            df_ = []
+            if state == "init":
+                f0_inc = 0
+                f1_rot = 0
                 fbs_ = fastpli.objects.fiber_bundles.Cut(
-                    fbs_, [[-30] * 3, [30] * 3])
+                    fbs, [[-30] * 3, [30] * 3])
                 phi, theta = fastpli.analysis.orientation.fiber_bundles(fbs_)
 
                 df_.append(
@@ -63,10 +66,32 @@ with tqdm(total=len(parameters) * len(f0_incs)) as pbar:
                                      "omega", "psi", "f0_inc", "f1_rot", "phi",
                                      "theta"
                                  ]))
-            pbar.update()
+                pbar.update(len(f0_incs))
+            else:
+                for f0_inc in f0_incs:
+                    for f1_rot in df_sub[df_sub.f0_inc == f0_inc].f1_rot.unique():
+                        rot_inc = fastpli.tools.rotation.y(-np.deg2rad(f0_inc))
+                        rot_phi = fastpli.tools.rotation.x(np.deg2rad(f1_rot))
+                        rot = np.dot(rot_inc, rot_phi)
+                        fbs_ = fastpli.objects.fiber_bundles.Rotate(fbs, rot)
+                        fbs_ = fastpli.objects.fiber_bundles.Cut(
+                            fbs_, [[-30] * 3, [30] * 3])
+                        phi, theta = fastpli.analysis.orientation.fiber_bundles(fbs_)
 
-        df_ = pd.concat(df_, ignore_index=True)
-        df_.to_pickle(
-            f"output/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.solved.pkl"
-        )
-        gc.collect()
+                        df_.append(
+                            pd.DataFrame([[
+                                omega, psi, f0_inc, f1_rot,
+                                phi.astype(np.float32).ravel(),
+                                theta.astype(np.float32).ravel()
+                            ]],
+                                         columns=[
+                                             "omega", "psi", "f0_inc", "f1_rot", "phi",
+                                             "theta"
+                                         ]))
+                    pbar.update()
+
+            df_ = pd.concat(df_, ignore_index=True)
+            df_.to_pickle(
+                f"output/models/cube_2pop_psi_{psi:.1f}_omega_{omega:.1f}_.{state}.pkl"
+            )
+            gc.collect()
