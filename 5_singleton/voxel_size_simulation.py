FIX rotation error, ADD analysis

5_singleton/voxel_size_simulation.py
@@ -97,7 +97,7 @@ def run(parameter):
     rnd_radii_0 = radius * np.random.lognormal(0, 0.1, seeds_0.shape[0])
     rnd_radii_1 = radius * np.random.lognormal(0, 0.1, seeds_1.shape[0])
 
-    vec = np.array([0, 0, 1])
+    vec = np.array([1, 0, 0])
     rot_inc = fastpli.tools.rotation.y(-np.deg2rad(f0_inc))
     rot_phi = fastpli.tools.rotation.x(np.deg2rad(f1_rot))
     rot = np.dot(rot_inc, rot_phi)
@@ -106,9 +106,9 @@ def run(parameter):
     fiber_bundles = [
         fastpli.model.sandbox.build.cuboid(
             p=-0.5 *
-            np.array([PIXEL_SIZE * 4, PIXEL_SIZE * 4, THICKNESS * 1.25]),
+            np.array([PIXEL_SIZE * 3, PIXEL_SIZE * 3, THICKNESS * 1.25]),
             q=0.5 *
-            np.array([PIXEL_SIZE * 4, PIXEL_SIZE * 4, THICKNESS * 1.25]),
+            np.array([PIXEL_SIZE * 3, PIXEL_SIZE * 3, THICKNESS * 1.25]),
             phi=np.deg2rad(0),
             theta=np.pi / 2 - np.deg2rad(f0_inc),
             seeds=seeds_0,
@@ -145,6 +145,19 @@ def run(parameter):
     for i in range(1000):
         if solver.step():
             break
+        if i % 50 == 0:
+            overlap = solver.overlap / solver.num_col_obj if solver.num_col_obj else 0
+            logger.info(
+                f"step: {i}, {solver.num_obj}/{solver.num_col_obj} {round(overlap * 100)}%"
+            )
+            solver.fiber_bundles = fastpli.objects.fiber_bundles.Cut(
+                solver.fiber_bundles, [
+                    -0.5 * np.array(
+                        [PIXEL_SIZE * 3, PIXEL_SIZE * 3, THICKNESS * 1.25]),
+                    0.5 *
+                    np.array([PIXEL_SIZE * 3, PIXEL_SIZE * 3, THICKNESS * 1.25])
+                ])
+
         # solver.draw_scene()
 
     end_time = time.time()
@@ -156,13 +169,13 @@ def run(parameter):
     with h5py.File(file_pref + '.solved.h5', 'w') as h5f:
         dset = h5f.create_group(f'solver/')
         solver.save_h5(dset, script=open(os.path.abspath(__file__), 'r').read())
-        dset['/'].attrs['psi'] = psi
-        dset['/'].attrs['omega'] = omega
-        dset['/'].attrs['num_obj'] = solver.num_obj
-        dset['/'].attrs['num_steps'] = solver.num_steps
-        dset['/'].attrs['obj_mean_length'] = solver.obj_mean_length
-        dset['/'].attrs['obj_min_radius'] = solver.obj_min_radius
-        dset['/'].attrs['time'] = end_time - start_time
+        dset.attrs['psi'] = psi
+        dset.attrs['omega'] = omega
+        dset.attrs['num_obj'] = solver.num_obj
+        dset.attrs['num_steps'] = solver.num_steps
+        dset.attrs['obj_mean_length'] = solver.obj_mean_length
+        dset.attrs['obj_min_radius'] = solver.obj_min_radius
+        dset.attrs['time'] = end_time - start_time
 
         logger.info(f"generation done")
 
@@ -187,8 +200,9 @@ def run(parameter):
             simpli.set_voi(-0.5 * np.array([PIXEL_SIZE, PIXEL_SIZE, THICKNESS]),
                            0.5 * np.array([PIXEL_SIZE, PIXEL_SIZE, THICKNESS]))
 
-            if simpli.memory_usage() > 1e3:
+            if simpli.memory_usage() * args.num_proc > 200000:
                 print(str(round(simpli.memory_usage(), 2)) + 'MB')
+                return
 
             for dn, model in [(-0.001, 'p'), (0.002, 'r')]:
                 dset = h5f.create_group(f'simpli/{voxel_size}/{model}')
@@ -220,8 +234,7 @@ def run(parameter):
                     # calculate modalities
                     epa = simpli.apply_epa(images)
                     dset['analysis/epa/' + str(t) + '/transmittance'] = epa[0]
-                    dset['analysis/epa/' + str(t) + '/direction'] = np.rad2deg(
-                        epa[1])
+                    dset['analysis/epa/' + str(t) + '/direction'] = epa[1]
                     dset['analysis/epa/' + str(t) + '/retardation'] = epa[2]
 
                 del label_field

5_singleton/voxel_size_simulation_init_0.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
@@ -10,6 +9,8 @@ import os
 
 from tqdm import tqdm
 import helper.circular
+import fastpli.analysis
+import fastpli.io
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i",
@@ -36,21 +37,46 @@ def run(file):
 
     df = []
     with h5py.File(file, 'r') as h5f:
+        fbs = fastpli.io.fiber_bundles.load_h5(h5f["solver/"])
+        fbs = fastpli.objects.fiber_bundles.Cut(
+            fbs, [[-pixel_size / 2, -pixel_size / 2, -30],
+                  [pixel_size / 2, pixel_size / 2, 30]])
+        phi, theta = fastpli.analysis.orientation.fiber_bundles(fbs)
+
+        dset = h5f['solver']
+        psi = dset.attrs['psi']
+        omega = dset.attrs['omega']
+        num_obj = dset.attrs['num_obj']
+        num_steps = dset.attrs['num_steps']
+        obj_mean_length = dset.attrs['obj_mean_length']
+        obj_min_radius = dset.attrs['obj_min_radius']
+        time = dset.attrs['time']
+
         for voxel_size in h5f['simpli']:
             for model in h5f[f'simpli/{voxel_size}']:
                 h5f_sub = h5f[f'simpli/{voxel_size}/{model}']
 
-                df.append(pd.DataFrame([[omega, psi, f0_inc, f1_rot,    radius, pixel_size,
-                                        h5f_sub['simulation/data/0'][...].ravel(),
-                                        h5f_sub['simulation/optic/0'][...].ravel(),
-                                        h5f_sub['analysis/epa/0/transmittance'][...].ravel(),
-                                        np.deg2rad(h5f_sub['analysis/epa/0/direction'][...].ravel()),
-                                        h5f_sub['analysis/epa/0/retardation'][...].ravel()]],
-                                        columns=[
-                                        "omega", "psi", "f0_inc", "f1_rot",
-                                        "radius", "pixel_size", "data","optic",
-                                        "epa_trans", "epa_dir", "epa_ret"
-                                    ]))
+                df.append(
+                    pd.DataFrame([[
+                        float(voxel_size), model, omega, psi, f0_inc, f1_rot,
+                        radius, pixel_size,
+                        h5f_sub['simulation/data/0'][...].ravel(),
+                        h5f_sub['simulation/optic/0'][...].ravel(),
+                        h5f_sub['analysis/epa/0/transmittance'][...].ravel(),
+                        h5f_sub['analysis/epa/0/direction'][...].ravel(),
+                        h5f_sub['analysis/epa/0/retardation'][...].ravel(), phi,
+                        theta, num_obj, num_steps, obj_mean_length,
+                        obj_min_radius, time
+                    ]],
+                                 columns=[
+                                     "voxel_size", "model", "omega", "psi",
+                                     "f0_inc", "f1_rot", "radius", "pixel_size",
+                                     "data", "optic", "epa_trans", "epa_dir",
+                                     "epa_ret", "f_phi", "f_theta",
+                                     "solver.num_obj", "solver.num_steps",
+                                     "solver.obj_mean_length",
+                                     "solver.obj_min_radius", "solver.time"
+                                 ]))
 
     return df
 
@@ -59,7 +85,10 @@ files = glob.glob(os.path.join(args.input, "*.h5"))
 
 run.num_p = args.num_proc
 with mp.Pool(processes=run.num_p) as pool:
-    df = [item for sub in tqdm(pool.imap_unordered(run, files), total=len(files)) for item in sub ]
+    df = [
+        item for sub in tqdm(pool.imap_unordered(run, files), total=len(files))
+        for item in sub
+    ]
     df = pd.concat(df, ignore_index=True)
 
 df.to_pickle(os.path.join(args.input, "voxel_size_simulation.pkl"))