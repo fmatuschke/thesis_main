d1edd1a62ba0105fe218f25598ef7d6bf0321822
fmatuschke <f.matuschke@fz-juelich.de>
2020-07-20 15:10:32 +0200

    ADD 5_ tikz pdf

--- 5_singleton/voxel_size_simulation_analysis.py
+++ 5_singleton/voxel_size_simulation_analysis.py
@@ -93,6 +93,8 @@ def to_tikz(data,
             f"    table [x=x, y=y, col sep=comma] {{{file_name}_0.dat}};\n" \
             "\\addplot [thick, red, dashed]\n" \
             f"    table [x=x, y=y, col sep=comma] {{{file_name}_2.dat}};\n" \
+            "\\addplot [thick, blue, dash dot]\n" \
+            f"    table [x=x, y=y, col sep=comma] {{{file_name}_4.dat}};\n" \
             "\\end{polaraxis}\n"
         )
         f.write("%\n")
@@ -110,6 +112,8 @@ def to_tikz(data,
             f"    table [x=x, y=y, col sep=comma] {{{file_name}_1.dat}};\n" \
             "\\addplot [thick, red, dashed]\n" \
             f"    table [x=x, y=y, col sep=comma] {{{file_name}_3.dat}};\n" \
+            "\\addplot [thick, blue, dash dot]\n" \
+            f"    table [x=x, y=y, col sep=comma] {{{file_name}_5.dat}};\n" \
             "\\end{polaraxis}\n"
             "\\end{scope}\n" \
         )
@@ -131,51 +135,59 @@ if __name__ == "__main__":
     hist_phi_inc = lambda n: np.linspace(
         np.pi / 2, 3 * np.pi / 2, n + 1, endpoint=True)
     df = pd.read_pickle(args.input)
-    for model in df.model.unique():
-        for f0_inc in sorted(df.f0_inc.unique()):
-            for vs in sorted(df.voxel_size.unique()):
-                sub = (df.omega == 0.0) & (df.psi == 1.0) & (
-                    df.f1_rot == 0.0) & (df.voxel_size == vs) & (
-                        df.f0_inc == f0_inc) & (df.model == model)
-                data = []
-                df_sub = df[sub]
-                if len(df_sub) > 1:
-                    print("FOO")
-                    exit()
-                if len(df_sub) == 0:
-                    continue
+    for f0_inc in sorted(df.f0_inc.unique()):
+        for vs in sorted(df.voxel_size.unique()):
+            sub = (df.omega == 0.0) & (df.psi == 1.0) & (df.f1_rot == 0.0) & (
+                df.voxel_size == vs) & (df.f0_inc == f0_inc)
+            data = []
+            df_sub = df[sub]
+            if len(df_sub) != 2:
+                print("FOO")
+                exit()
 
-                phi = df_sub.explode("f_phi").f_phi.to_numpy(float)
-                theta = df_sub.explode("f_theta").f_theta.to_numpy(float)
-                phi, phi_inc = helper.circular.orientation_sph_plot(phi, theta)
-                if np.any(np.isnan(phi)) or np.any(np.isnan(phi_inc)):
-                    print(phi, phi_inc)
-                h, x = np.histogram(phi, hist_phi(4 * 18), density=True)
-                if np.any(np.isnan(h)) or np.any(np.isnan(x)):
-                    print("1:", h, x)
-                x = x[:-1] + (x[1] - x[0]) / 2
-                h = h / np.amax(h)
-                data.append(np.vstack((np.rad2deg(x), h)))
-                h, x = np.histogram(phi_inc, hist_phi_inc(4 * 18), density=True)
-                if np.any(np.isnan(h)) or np.any(np.isnan(x)):
-                    print("2:", h, x)
-                x = x[:-1] + (x[1] - x[0]) / 2
-                h = h / np.amax(h)
-                data.append(np.vstack((np.rad2deg(x), h)))
+            phi = df_sub.explode("f_phi").f_phi.to_numpy(float)
+            theta = df_sub.explode("f_theta").f_theta.to_numpy(float)
+            phi, phi_inc = helper.circular.orientation_sph_plot(phi, theta)
+            if np.any(np.isnan(phi)) or np.any(np.isnan(phi_inc)):
+                print(phi, phi_inc)
+            h, x = np.histogram(phi, hist_phi(4 * 18), density=True)
+            if np.any(np.isnan(h)) or np.any(np.isnan(x)):
+                print("1:", h, x)
+            x = x[:-1] + (x[1] - x[0]) / 2
+            h = h / np.amax(h)
+            data.append(np.vstack((np.rad2deg(x), h)))
+            h, x = np.histogram(phi_inc, hist_phi_inc(4 * 18), density=True)
+            if np.any(np.isnan(h)) or np.any(np.isnan(x)):
+                print("2:", h, x)
+            x = x[:-1] + (x[1] - x[0]) / 2
+            h = h / np.amax(h)
+            data.append(np.vstack((np.rad2deg(x), h)))
 
-                phi = df_sub.explode("epa_dir").epa_dir.to_numpy(float)
-                phi[phi > np.pi] -= 2 * np.pi
-                phi = phi[0]
-                data.append(
-                    np.vstack(([np.rad2deg(phi),
-                                np.rad2deg(phi)], [0, 1])))
-                data.append(np.vstack(([180 - f0_inc, 180 - f0_inc], [0, 1])))
+            phi = df_sub[df_sub.model == 'r'].explode(
+                "epa_dir").epa_dir.to_numpy(float)
+            phi[phi > np.pi] -= 2 * np.pi
+            phi[phi > np.pi / 2] -= np.pi
+            h, x = np.histogram(phi, hist_phi(4 * 18), density=True)
+            x = x[:-1] + (x[1] - x[0]) / 2
+            h = h / np.amax(h)
+            data.append(np.vstack((np.rad2deg(x), h)))
+            data.append(np.vstack(([180 - f0_inc, 180 - f0_inc], [0, 1])))
 
-                to_tikz(
-                    data,
-                    os.path.join(
-                        out_path,
-                        f"voxel_size_simulation_f0_inc_{f0_inc:.2f}_vs_{vs:.4f}.tikz"
-                    ),
-                    path_to_data="\\currfiledir",
-                    standalone=False)
+            phi = df_sub[df_sub.model == 'p'].explode(
+                "epa_dir").epa_dir.to_numpy(float)
+            phi[phi > np.pi] -= 2 * np.pi
+            phi[phi > np.pi / 2] -= np.pi
+            h, x = np.histogram(phi, hist_phi(4 * 18), density=True)
+            x = x[:-1] + (x[1] - x[0]) / 2
+            h = h / np.amax(h)
+            data.append(np.vstack((np.rad2deg(x), h)))
+            data.append(np.vstack(([180 - f0_inc, 180 - f0_inc], [0, 1])))
+
+            to_tikz(
+                data,
+                os.path.join(
+                    out_path,
+                    f"voxel_size_simulation_f0_inc_{f0_inc:.2f}_vs_{vs:.4f}.tikz"
+                ),
+                path_to_data="\\currfiledir",
+                standalone=False)
--- 5_singleton/voxel_size_simulation_analysis.sh
+++ 5_singleton/voxel_size_simulation_analysis.sh
@@ -4,6 +4,7 @@ set -e
 # python3 voxel_size_simulation.py -i ../data/models/1_rnd_seed/cube_2pop_psi_1.00_omega_0.00_r_1.00_v0_210_.solved.h5 -o output/vs_0 -p 1
 python3 voxel_size_simulation_analysis.py -i output/vs_0/voxel_size_simulation.pkl
 mkdir -p output/tmp
+mkdir -p output/tmp/output/tmp
 pdflatex -halt-on-error -synctex=1 -interaction=nonstopmode --shell-escape -output-directory output/tmp voxel_size_simulation_analysis.tex 
 cp output/tmp/voxel_size_simulation_analysis.pdf output/images/voxel_size_simulation_analysis.pdf
 xdg-open output/images/voxel_size_simulation_analysis.pdf
