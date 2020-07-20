2fb1f2e70602df14126ef4bc82447e993e65427b
fmatuschke <f.matuschke@fz-juelich.de>
2020-07-20 13:52:11 +0200

    5_ tikz pdf

--- 0_core/helper/circular.py
+++ 0_core/helper/circular.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy.stats import circmean
+from fastpli.analysis.orientation import remap_orientation
 
 
 def remap(data, high=2 * np.pi, low=0):
+    high, low = max(high, low), min(high, low)
     data = np.array(data, copy=True)
     data -= low
     data %= (high - low)
@@ -15,3 +17,25 @@ def center_mean(data, high=2 * np.pi, low=0, margin=0):
     cm = circmean(data, high, low)
     data = remap(data, cm - (high - low) / 2, cm + (high - low) / 2)
     return data
+
+
+def orientation_sph_plot(phi, theta):
+    # rotate phi [0,360), theta [0,90) to [-90, 90), [0,180)
+    phi, theta = remap_orientation(phi, theta)
+
+    # phi > 180 to [-180,180)
+    mask = phi >= np.pi
+    phi[mask] -= 2 * np.pi
+
+    # phi > 90, phi < 90 to theta > 90
+    mask = phi >= np.pi / 2
+    theta[mask] = np.pi - theta[mask]
+    phi[mask] -= np.pi
+    mask = phi < -np.pi / 2
+    theta[mask] = np.pi - theta[mask]
+    phi[mask] += np.pi
+    theta[theta >= np.pi] -= np.pi
+
+    phi_theta = np.pi - (np.pi / 2 - theta)
+
+    return phi, phi_theta
--- 5_singleton/voxel_size_simulation_analysis.py
+++ 5_singleton/voxel_size_simulation_analysis.py
@@ -126,58 +126,50 @@ if __name__ == "__main__":
     out_path = "output/images/voxel_size_simulation"
     os.makedirs(out_path, exist_ok=True)
 
-    hist_bin = lambda n: np.linspace(0, np.pi, n + 1, endpoint=True)
+    hist_phi = lambda n: np.linspace(
+        -np.pi / 2, np.pi / 2, n + 1, endpoint=True)
+    hist_phi_inc = lambda n: np.linspace(
+        np.pi / 2, 3 * np.pi / 2, n + 1, endpoint=True)
     df = pd.read_pickle(args.input)
     for model in df.model.unique():
-        fig, axs = plt.subplots(len(df.f0_inc.unique()),
-                                len(df.voxel_size.unique()),
-                                subplot_kw={'projection': 'polar'},
-                                figsize=(20, 20))
-        axs = np.atleast_2d(axs)
-        for i, f0_inc in enumerate(sorted(df.f0_inc.unique())):
-            for j, vs in enumerate(sorted(df.voxel_size.unique())):
+        for f0_inc in sorted(df.f0_inc.unique()):
+            for vs in sorted(df.voxel_size.unique()):
                 sub = (df.omega == 0.0) & (df.psi == 1.0) & (
                     df.f1_rot == 0.0) & (df.voxel_size == vs) & (
                         df.f0_inc == f0_inc) & (df.model == model)
                 data = []
                 df_sub = df[sub]
                 if len(df_sub) > 1:
-                    print("fooo", omega, psi)
+                    print("FOO")
+                    exit()
                 if len(df_sub) == 0:
                     continue
 
                 phi = df_sub.explode("f_phi").f_phi.to_numpy(float)
                 theta = df_sub.explode("f_theta").f_theta.to_numpy(float)
-
-                theta[phi > np.pi] = np.pi - theta[phi > np.pi]
-                phi = helper.circular.remap(phi, np.pi, 0)
-
-                h, x = np.histogram(phi, hist_bin(4 * 18), density=True)
+                phi, phi_inc = helper.circular.orientation_sph_plot(phi, theta)
+                if np.any(np.isnan(phi)) or np.any(np.isnan(phi_inc)):
+                    print(phi, phi_inc)
+                h, x = np.histogram(phi, hist_phi(4 * 18), density=True)
+                if np.any(np.isnan(h)) or np.any(np.isnan(x)):
+                    print("1:", h, x)
                 x = x[:-1] + (x[1] - x[0]) / 2
-                x = np.append(np.concatenate((x, x + np.pi), axis=0), x[0])
-                h = np.append(np.concatenate((h, h), axis=0), h[0])
-                h = h / np.max(h)
+                h = h / np.amax(h)
                 data.append(np.vstack((np.rad2deg(x), h)))
-
-                h, x = np.histogram(theta, hist_bin(4 * 18), density=True)
-                x = np.pi - (np.pi / 2 -
-                             (x[:-1] +
-                              (x[1] - x[0]) / 2))  # np/2 for incl, - for plot
-                h = h / np.max(h)
+                h, x = np.histogram(phi_inc, hist_phi_inc(4 * 18), density=True)
+                if np.any(np.isnan(h)) or np.any(np.isnan(x)):
+                    print("2:", h, x)
+                x = x[:-1] + (x[1] - x[0]) / 2
+                h = h / np.amax(h)
                 data.append(np.vstack((np.rad2deg(x), h)))
 
                 phi = df_sub.explode("epa_dir").epa_dir.to_numpy(float)
-                phi = helper.circular.remap(phi, np.pi, 0)
-
-                h, x = np.histogram(phi, hist_bin(4 * 18), density=True)
-                x = x[:-1] + (x[1] - x[0]) / 2
-                x = np.append(np.concatenate((x, x + np.pi), axis=0), x[0])
-                h = np.append(np.concatenate((h, h), axis=0), h[0])
-                h = h / np.max(h)
-                data.append(np.vstack((np.rad2deg(x), h)))
-
-                # theta data
-                data.append(np.vstack(([0, f0_inc], [0, 1])))
+                phi[phi > np.pi] -= 2 * np.pi
+                phi = phi[0]
+                data.append(
+                    np.vstack(([np.rad2deg(phi),
+                                np.rad2deg(phi)], [0, 1])))
+                data.append(np.vstack(([180 - f0_inc, 180 - f0_inc], [0, 1])))
 
                 to_tikz(
                     data,
