36c530b21b95721a8e8ee3124e22466d2dacb51b
fmatuschke <f.matuschke@fz-juelich.de>
2020-07-31 13:01:30 +0200

    ADD replace.sh

--- 2_cube_factory/cube_2pop_statistic_jureca.sh
+++ 2_cube_factory/cube_2pop_statistic_jureca.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
-#SBATCH --nodes=10
-#SBATCH --ntasks=240
-#SBATCH --nasks-per-node=24
+#SBATCH --nodes=$2
+#SBATCH --ntasks=$1
+#SBATCH --tasks-per-node=24
 #SBATCH --cpus-per-task=1
-#SBATCH --time=24:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=gpus
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=thesis-cube_2pop_stat
@@ -12,16 +12,12 @@
 #SBATCH --mail-user=f.matuschke@fz-juelich.de
 #SBATCH --mail-type=ALL
 
-# source ../jureca_modules.sh
+# source /p/home/jusers/matuschke1/jureca/private/thesis/jureca_modules.sh
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 0
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 240
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 480
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 720
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 960
+# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 1200
 
-srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 0
-
-# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 240
-
-# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 480
-
-# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 720
-
-# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 960
-
-# srun -n 50 /p/home/jusers/matuschke1/jureca/private/thesis/env-jureca/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat -n 10000 --start 1200
+srun -n $1 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python3 -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start $3
---
+++ 2_cube_factory/replace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+c=0
+file=$(cat $1)
+for var in "$@"; do
+   if (($c == 0)); then
+      ((c++))
+      continue
+   fi
+   file=$(sed 's/$'"$c"'/'"$var"'/g' <<<$file)
+
+   ((c++))
+done
+
+echo "$file" >$1.run
+echo "$file"
+
+# ./run_sbatch.sh $1.run
+# rm $1.run
