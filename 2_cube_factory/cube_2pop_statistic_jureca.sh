73cfd201d80d10dfd2eddf2aec883f65fe747ae7
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-01 07:56:11 +0200

    RFC 2_ jureca code

--- 2_cube_factory/cube_2pop_statistic_jureca.sh
+++ 2_cube_factory/cube_2pop_statistic_jureca.sh
@@ -12,12 +12,13 @@
 #SBATCH --mail-user=f.matuschke@fz-juelich.de
 #SBATCH --mail-type=ALL
 
-# source /p/home/jusers/matuschke1/jureca/private/thesis/jureca_modules.sh
+source /p/home/jusers/matuschke1/jureca/private/thesis/jureca_modules.sh
+
 # srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 0
 # srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 240
 # srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 480
 # srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 720
 # srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 960
-# srun -n 240 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 1200
+# srun -n 50 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start 1200
 
 srun -n $1 /p/home/jusers/matuschke1/jureca/private/thesis/env/bin/python3 -m mpi4py cube_2pop_statistic_jureca.py -o /p/scratch/cjinm11/matuschke1/thesis/2/cube_2pop_stat_ -n 10000 --start $3
