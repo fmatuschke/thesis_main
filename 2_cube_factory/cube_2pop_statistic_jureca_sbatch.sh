2a4488ab301fbe4c2bd63f7f160151804774bb3f
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-04 12:35:20 +0200

    FIX time

--- 2_cube_factory/cube_2pop_statistic_jureca_sbatch.sh
+++ 2_cube_factory/cube_2pop_statistic_jureca_sbatch.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 0 0:10:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 120 0:10:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 240 0:10:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 360 0:30:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 480 0:30:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 600 1:00:00
-./replace.sh cube_2pop_statistic_jureca.sh 120 5 720 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 0 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 120 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 240 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 360 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 480 1:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 600 2:00:00
+./replace.sh cube_2pop_statistic_jureca.sh 120 5 720 2:00:00
 ./replace.sh cube_2pop_statistic_jureca.sh 120 5 840 2:00:00
 ./replace.sh cube_2pop_statistic_jureca.sh 120 5 960 5:00:00
 ./replace.sh cube_2pop_statistic_jureca.sh 120 5 1080 10:00:00
