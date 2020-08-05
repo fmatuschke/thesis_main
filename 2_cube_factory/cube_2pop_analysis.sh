25d4feb6e6f14abe581bc08fc770699fb6fbab1f
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-05 18:57:49 +0200

    RFC 2_ tikz

--- 2_cube_factory/cube_2pop_analysis.sh
+++ 2_cube_factory/cube_2pop_analysis.sh
@@ -6,9 +6,9 @@ mkdir -p output/tmp/output/tmp
 mkdir -p output/tikz
 
 rm -f output/tmp/cube_2pop_analysis.pdf
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp cube_2pop_analysis.tex
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp --shell-escape cube_2pop_analysis.tex
 mv output/tmp/cube_2pop_analysis.pdf output/tikz/cube_2pop_analysis.pdf
 
-rm -f output/tmp/cube_2pop_images.pdf
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp cube_2pop_images.tex
-mv output/tmp/cube_2pop_images.pdf output/tikz/cube_2pop_images.pdf
+# rm -f output/tmp/cube_2pop_images.pdf
+# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp cube_2pop_images.tex
+# mv output/tmp/cube_2pop_images.pdf output/tikz/cube_2pop_images.pdf
