3d09de0a1c2a00ae06c4e0d05d86f96410721ca5
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-17 11:40:17 +0200

    FIX

--- 2_cube_factory/cube_2pop_juron_sbatch.sh
+++ 2_cube_factory/cube_2pop_juron_sbatch.sh
@@ -7,20 +7,33 @@ n=20
 num_p=80
 sum=0
 
-for r v in {210,1,105,0.5}
+# for r v in {210,1,105,0.5}
+# do
+for ((i=0; i<$N; i=i+$num_p))
 do
-   for ((i=0; i<$N; i=i+$num_p))
-   do
-      if [ $(($i+$num_p)) -gt $(($N-1)) ]
-      then
-         num_p=$(($N-$i+1))
-         ./juron_replace.sh parameter_statistic_juron.sh $num_p $i $r $v
-         ((sum+=num_p))
-         break
-      fi
-      ./juron_replace.sh parameter_statistic_juron.sh $num_p $i $r $v
+   if [ $(($i+$num_p)) -gt $(($N-1)) ]
+   then
+      num_p=$(($N-$i+1))
+      ./juron_replace.sh parameter_statistic_juron.sh $num_p $i 1 210
       ((sum+=num_p))
-   done
+      break
+   fi
+   ./juron_replace.sh parameter_statistic_juron.sh $num_p $i 1 210
+   ((sum+=num_p))
+done
+# done
+
+for ((i=0; i<$N; i=i+$num_p))
+do
+   if [ $(($i+$num_p)) -gt $(($N-1)) ]
+   then
+      num_p=$(($N-$i+1))
+      ./juron_replace.sh parameter_statistic_juron.sh $num_p $i 0.5 105
+      ((sum+=num_p))
+      break
+   fi
+   ./juron_replace.sh parameter_statistic_juron.sh $num_p $i 0.5 105
+   ((sum+=num_p))
 done
 
 echo $sum
