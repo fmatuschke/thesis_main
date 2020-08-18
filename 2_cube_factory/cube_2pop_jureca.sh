040998ec79c0e5d3b1e99078da6385cc0053cb7a
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-18 10:24:09 +0200

    RFC jureca modules

--- 2_cube_factory/cube_2pop_jureca.sh
+++ 2_cube_factory/cube_2pop_jureca.sh
@@ -6,17 +6,16 @@
 #SBATCH --output=mpi-out.%j
 #SBATCH --error=mpi-err.%j
 #SBATCH --time=24:00:00
-#SBATCH --partition=gpus
-#SBATCH --gres=gpu:1
+#SBATCH --partition=batch
 #SBATCH --mail-user=f.matuschke@fz-juelich.de
 #SBATCH --mail-type=ALL
 
+srun -n 96 /p/project/cjinm11/Private/matuschke1/thesis/env/bin/python3 \
+   -u -m mpi4py cube_2pop_sc.py \
+   -o /p/scratch/cjinm11/matuschke1/thesis/2/output/cube_2pop_2/ \
+   -r 1.0 -v 210 -n 10000 -p 4
+
 # srun -n 96 /p/project/cjinm11/Private/matuschke1/thesis/env/bin/python3 \
 #    -u -m mpi4py cube_2pop.py \
-#    -o /p/scratch/cjinm11/matuschke1/thesis/2/output/cube_2pop_0/ \
-#    -r 1.0 -v 210 -n 10000 -p 4
-
-srun -n 96 /p/project/cjinm11/Private/matuschke1/thesis/env/bin/python3 \
-   -u -m mpi4py cube_2pop.py \
-   -o /p/scratch/cjinm11/matuschke1/thesis/2/output/cube_2pop_0/ \
-   -r 0.5 -v 105 -n 10000 -p 4   
+#    -o /p/scratch/cjinm11/matuschke1/thesis/2/output/cube_2pop_2/ \
+#    -r 0.5 -v 105 -n 10000 -p 4   
--- jureca_modules.sh
+++ jureca_modules.sh
@@ -6,8 +6,10 @@
 # module load CMake
 
 module load Intel/2019.3.199-GCC-8.3.0
-module load Python
 module load IntelMPI/2019.3.199
-module load mpi4py
-module load HDF5
+module load SciPy-Stack/2019a-Python-3.6.8
+module load numba/0.43.1-Python-3.6.8
+module load mpi4py/3.0.1-Python-3.6.8
+module load SciPy-Stack/2019a-Python-3.6.8
+module load h5py/2.9.0-Python-3.6.8
 module load CMake
