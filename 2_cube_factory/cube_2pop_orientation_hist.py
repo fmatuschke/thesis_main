707ab5961c7bb64c2ec58b3550e1870347c3141c
fmatuschke <f.matuschke@fz-juelich.de>
2020-10-19 15:27:21 +0200

    ADD 2d orientation hist

--- 2_cube_factory/cube_2pop_orientation_hist.py
+++ 2_cube_factory/cube_2pop_orientation_hist.py
@@ -12,6 +12,7 @@ import tqdm
 import pandas as pd
 
 import fibers
+import fastpli.analysis
 
 # arguments
 parser = argparse.ArgumentParser()
@@ -34,6 +35,9 @@ hist_polar_bin = lambda n: np.linspace(
 
 
 def polar_hist(phi, theta):
+    phi = np.array(phi)
+    theta = np.array(theta)
+
     incl = np.pi / 2 - theta
 
     # remap to phi->[-np.pi/2, np.pi/2], incl->[-np.pi/2, np.pi/2]
@@ -65,6 +69,9 @@ def run(df):
     phi, theta = fibers.ori_from_file(file, 0, 0, 60)
     phi_x, phi_h, incl_x, incl_h = polar_hist(phi, theta)
 
+    # 2d hist
+    h, x, y, _ = fastpli.analysis.orientation.histogram(phi, theta)
+
     # INIT
     file = file.replace("solved", "init")
     file = file.replace("tmp", "init")
@@ -72,39 +79,24 @@ def run(df):
     phi_init_x, phi_init_h, incl_init_x, incl_init_h = polar_hist(phi, theta)
 
     return pd.DataFrame([[
-        df.omega,
-        df.psi,
-        df.r,
-        phi_x,
-        phi_h,
-        incl_x,
-        incl_h,
-        phi_init_x,
-        phi_init_h,
-        incl_init_x,
-        incl_init_h,
+        df.omega, df.psi, df.r, phi_x, phi_h, incl_x, incl_h, phi_init_x,
+        phi_init_h, incl_init_x, incl_init_h, h, x, y
     ]],
                         columns=[
-                            "omega",
-                            "psi",
-                            "r",
-                            "phi_x",
-                            "phi_h",
-                            "incl_x",
-                            "incl_h",
-                            "phi_init_x",
-                            "phi_init_h",
-                            "incl_init_x",
-                            "incl_init_h",
+                            "omega", "psi", "r", "phi_x", "phi_h", "incl_x",
+                            "incl_h", "phi_init_x", "phi_init_h", "incl_init_x",
+                            "incl_init_h", "hist_2d_h", "hist_2d_x", "hist_2d_y"
                         ])
 
 
 if __name__ == "__main__":
 
-    if not os.path.isfile(os.path.join(args.input, "hist", "cube_2pop.pkl")):
+    if False or not os.path.isfile(
+            os.path.join(args.input, "hist", "cube_2pop.pkl")):
         df = pd.read_pickle(os.path.join(args.input, "cube_2pop.pkl"))
         df = df[df.state != "init"]
         df = [df.iloc[i] for i in range(df.shape[0])]
+        # df = [df[0]]  # debug
 
         with mp.Pool(processes=args.num_proc) as pool:
             df = [
@@ -127,6 +119,35 @@ if __name__ == "__main__":
                     warnings.warn(f"len(df_sub) != 1: {r} {omega} {psi}")
                     continue
 
+                # 2d hist
+                with open(
+                        os.path.join(args.input, "hist",
+                                     f"cube_stats_p_{psi}_o_{omega}_r_{r}.dat"),
+                        "w") as f:
+
+                    H = df_sub.hist_2d_h.iloc[0]
+                    x_axis = df_sub.hist_2d_x.iloc[0]
+                    y_axis = df_sub.hist_2d_y.iloc[0]
+
+                    x_axis = x_axis + (x_axis[1] - x_axis[0]) / 2
+                    y_axis = y_axis[1:] + (y_axis[1] - y_axis[0]) / 2
+                    # y_axis = y_axis + (y_axis[1] - y_axis[0]) / 2
+                    H = np.vstack([H, H[0, :]])
+                    # H = np.hstack(
+                    #     [H, np.roll(H[:, 0], H.shape[1] // 2)[:, None]])
+                    H = H / np.sum(H.ravel())
+
+                    X, Y = np.meshgrid(np.rad2deg(x_axis), np.rad2deg(y_axis))
+
+                    # print(X.shape)
+                    # print(Y.shape)
+                    # print(H.shape)
+                    for h_array, x_array, y_array in zip(H.T, X, Y):
+                        for h, x, y in zip(h_array, x_array, y_array):
+                            f.write(f'{x:.2f} {y:.2f} {h:.6f}\n')
+                        f.write('\n')
+
+                # polar hist
                 df_[f"x"] = np.rad2deg(df_sub.phi_x.iloc[0])
                 df_[f"r_{r:.2f}_s_solved_phi_h"] = df_sub.phi_h.iloc[0]
                 df_[f"r_{r:.2f}_s_solved_incl_h"] = df_sub.incl_h.iloc[0]
