653ef1a69ece3e2ff789d1cfb2d5d1f9381887c9
fmatuschke <f.matuschke@fz-juelich.de>
2020-11-12 14:35:03 +0100

    FIX timing

--- 2_simulation/1_cubes/simulation_juron_run.sh
+++ 2_simulation/1_cubes/simulation_juron_run.sh
@@ -1,6 +1,6 @@
 for v in 1.00 0.50 2.00 5.00 10.00
 do
-   for start in 0 20 40 60 80
+   for start in {0..780..20}
    do
       file=$(cat simulation_juron.sh)
       file=$(sed 's/$1/0.125/g' <<<"$file")
