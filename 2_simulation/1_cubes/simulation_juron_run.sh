db7adda9de722a20965038feac5ac91c099017f0
fmatuschke <f.matuschke@fz-juelich.de>
2020-11-25 10:15:21 +0100

    ADJUST juron runner

--- 2_simulation/1_cubes/simulation_juron_run.sh
+++ 2_simulation/1_cubes/simulation_juron_run.sh
@@ -1,17 +1,11 @@
-for radius in 1.00 0.50 2.00 5.00 10.00
-do
-   for start in {0..779..20}
-   do
-      file=$(cat simulation_juron.sh)
-      file=$(sed 's/$1/0.125/g' <<<"$file")
-      file=$(sed 's/$2/'"$radius"'/g' <<<"$file")
+for start in {0..199..20}; do
+   file=$(cat simulation_juron.sh)
+   file=$(sed 's/$1/0.125/g' <<<"$file")
+   # file=$(sed 's/$2/'"$radius"'/g' <<<"$file")
+   file=$(sed 's/$3/'"$start"'/g' <<<"$file")
 
-
-      file=$(sed 's/$3/'"$start"'/g' <<<"$file")
-
-      echo "$file" >tmp.run
-      echo "$file"
-      bsub < tmp.run
-   done
+   echo "$file" >tmp.run
+   echo "$file"
+   #bsub < tmp.run
 done
-rm tmp.run
+#rm tmp.run
