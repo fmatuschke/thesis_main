e17f4c31ddb2372f8e27805482d1d5f15c7c8838
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-13 14:07:23 +0100

    ADD rofl_conf to simulation

--- 2_simulation/1_cubes/simulation_juron.py
+++ 2_simulation/1_cubes/simulation_juron.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
 
                             logger.info(f"tissue_pipeline: model:{model}")
 
-                            save = ['optic', 'epa', 'rofl']
+                            save = ['optic', 'epa', 'rofl', 'rofl_conf']
                             # save += ['tissue'] if m == 0 and name == 'LAP' else []
                             label_field, vector_field, tissue_properties = simpli.run_tissue_pipeline(
                                 h5f=dset, save=save)
