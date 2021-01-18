e2696e2c2fc552cdd3637be42f0dd2ae4a6975af
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-18 11:34:40 +0100

    RFC

--- 1_model/1_cubes/cube_2pop_orientation_hist2d.sh
+++ 1_model/1_cubes/cube_2pop_orientation_hist2d.sh
@@ -6,6 +6,6 @@ mkdir -p output/tmp/output/tmp
 mkdir -p output/tikz
 
 lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp --shell-escape cube_2pop_orientation_hist2d.tex
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp --shell-escape cube_2pop_orientation_hist2d.tex
+# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp --shell-escape cube_2pop_orientation_hist2d.tex
 mv output/tmp/cube_2pop_orientation_hist2d.pdf output/tikz/cube_2pop_orientation_hist2d.pdf
-xdg-open output/tikz/cube_2pop_orientation_hist2d.pdf &> /dev/null 2>&1
+xdg-open output/tikz/cube_2pop_orientation_hist2d.pdf &>/dev/null 2>&1
