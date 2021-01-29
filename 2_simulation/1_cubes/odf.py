4e298d6f75004cfdf4b399fe9bd4fbc5fae10292
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-29 15:02:26 +0100

    ADD odf plotting example

--- 2_simulation/1_cubes/odf.py
+++ 2_simulation/1_cubes/odf.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import helper.spherical_harmonics
+import fastpli.analysis
 
-n_theta = 42
+n_theta = 21
 n_phi = 42
 max_band = 4
 
@@ -10,14 +11,24 @@ max_band = 4
 data = np.zeros(helper.spherical_harmonics._num_values(max_band))
 data[0] = 1
 
+# dummy
+phi = np.array([0, 0, 0, 0, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2])
+theta = np.array([
+    np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2,
+    np.pi / 2
+])
+data = helper.spherical_harmonics.real_spherical_harmonics(
+    phi + np.random.uniform(-0.5, 0.5, phi.size),
+    theta + np.random.uniform(-0.5, 0.5, phi.size), max_band)
+
 if data.size > helper.spherical_harmonics._num_values(max_band):
     raise ValueError("data band < vis band")
 
 odf = np.empty((n_phi, n_theta, 6), np.float32)
-for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, False)):
+for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, True)):
     cosphi = np.cos(phi)
     sinphi = np.sin(phi)
-    for theta_i, theta in enumerate(np.linspace(0, np.pi, n_theta, False)):
+    for theta_i, theta in enumerate(np.linspace(0, np.pi, n_theta, True)):
         costheta = np.cos(theta)
         sintheta = np.sin(theta)
 
@@ -32,12 +43,41 @@ for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, False)):
         x = r * sintheta * cosphi
         y = r * sintheta * sinphi
         z = r * costheta
-        color = np.random.uniform(0, 1, (3))
-        # color = colorsHSV(x, y, z)
+        color = fastpli.analysis.images._orientation_to_hsv(
+            phi, np.pi / 2 - theta) / 255
 
         odf[phi_i, theta_i, :] = [x, y, z, color[0], color[1], color[2]]
 
+# to dat file
+with open("odf_test.dat", "w") as file:
+    file.write(f"x y z rgb\n")
+    for i in range(odf.shape[0]):
+        for j in range(odf.shape[1]):
+            file.write(
+                f"{odf[i,j,0]:.2f} {odf[i,j,1]:.2f} {odf[i,j,2]:.2f} {odf[i,j,3]:.2f},{odf[i,j,4]:.2f},{odf[i,j,5]:.2f}\n"
+            )
+        file.write("\n")
+
+
 # plot
+def set_axes_equal(ax):
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
 
 fig = plt.figure()  # figsize=(10, 10)
 ax = fig.add_subplot(111, projection='3d')
@@ -46,4 +86,6 @@ X = odf[:, :, 0]
 Y = odf[:, :, 1]
 Z = odf[:, :, 2]
 
-ax.plot_surface(X, Y, Z)
+ax.plot_surface(X, Y, Z, facecolors=odf[:, :, 3:])
+set_axes_equal(ax)
+plt.show()
