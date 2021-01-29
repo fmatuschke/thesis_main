fc25d2327801ea60ea9764a4793d78c7d1eca7b5
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-29 15:06:47 +0100

    RFC odf to function

--- 2_simulation/1_cubes/odf.py
+++ 2_simulation/1_cubes/odf.py
@@ -3,89 +3,133 @@ import matplotlib.pyplot as plt
 import helper.spherical_harmonics
 import fastpli.analysis
 
-n_theta = 21
-n_phi = 42
-max_band = 4
 
-# dummy sphere
-data = np.zeros(helper.spherical_harmonics._num_values(max_band))
-data[0] = 1
+def table(file_name,
+          directions,
+          inclinations,
+          max_band=6,
+          n_theta=21,
+          n_phi=42):
+    data = helper.spherical_harmonics.real_spherical_harmonics(
+        directions, np.pi / 2 - inclinations, max_band)
 
-# dummy
-phi = np.array([0, 0, 0, 0, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2])
-theta = np.array([
-    np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2,
-    np.pi / 2
-])
-data = helper.spherical_harmonics.real_spherical_harmonics(
-    phi + np.random.uniform(-0.5, 0.5, phi.size),
-    theta + np.random.uniform(-0.5, 0.5, phi.size), max_band)
+    odf = np.empty((n_phi, n_theta, 6), np.float32)
+    for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, True)):
+        cosphi = np.cos(phi)
+        sinphi = np.sin(phi)
+        for theta_i, theta in enumerate(np.linspace(0, np.pi, n_theta, True)):
+            costheta = np.cos(theta)
+            sintheta = np.sin(theta)
 
-if data.size > helper.spherical_harmonics._num_values(max_band):
-    raise ValueError("data band < vis band")
+            r = 0
+            c = 0
+            for band in range(0, max_band + 1, 2):
+                for order in range(-band, band + 1):
+                    r += helper.spherical_harmonics._spherical_harmonics(
+                        band, order, costheta, sintheta, phi) * data[c]
+                    c += 1
 
-odf = np.empty((n_phi, n_theta, 6), np.float32)
-for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, True)):
-    cosphi = np.cos(phi)
-    sinphi = np.sin(phi)
-    for theta_i, theta in enumerate(np.linspace(0, np.pi, n_theta, True)):
-        costheta = np.cos(theta)
-        sintheta = np.sin(theta)
+            x = r * sintheta * cosphi
+            y = r * sintheta * sinphi
+            z = r * costheta
+            color = fastpli.analysis.images._orientation_to_hsv(
+                phi, np.pi / 2 - theta) / 255
 
-        r = 0
-        c = 0
-        for band in range(0, max_band + 1, 2):
-            for order in range(-band, band + 1):
-                r += helper.spherical_harmonics._spherical_harmonics(
-                    band, order, costheta, sintheta, phi) * data[c]
-                c += 1
+            odf[phi_i, theta_i, :] = [x, y, z, color[0], color[1], color[2]]
 
-        x = r * sintheta * cosphi
-        y = r * sintheta * sinphi
-        z = r * costheta
-        color = fastpli.analysis.images._orientation_to_hsv(
-            phi, np.pi / 2 - theta) / 255
-
-        odf[phi_i, theta_i, :] = [x, y, z, color[0], color[1], color[2]]
-
-# to dat file
-with open("odf_test.dat", "w") as file:
-    file.write(f"x y z rgb\n")
-    for i in range(odf.shape[0]):
-        for j in range(odf.shape[1]):
-            file.write(
-                f"{odf[i,j,0]:.2f} {odf[i,j,1]:.2f} {odf[i,j,2]:.2f} {odf[i,j,3]:.2f},{odf[i,j,4]:.2f},{odf[i,j,5]:.2f}\n"
-            )
-        file.write("\n")
+    with open(file_name, "w") as file:
+        file.write(f"x y z rgb\n")
+        for i in range(odf.shape[0]):
+            for j in range(odf.shape[1]):
+                file.write(
+                    f"{odf[i,j,0]:.2f} {odf[i,j,1]:.2f} {odf[i,j,2]:.2f} {odf[i,j,3]:.2f},{odf[i,j,4]:.2f},{odf[i,j,5]:.2f}\n"
+                )
+            file.write("\n")
 
 
-# plot
-def set_axes_equal(ax):
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
+if __name__ == "__main__":
 
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
+    n_theta = 21
+    n_phi = 42
+    max_band = 4
 
-    plot_radius = 0.5 * max([x_range, y_range, z_range])
+    # dummy sphere
+    data = np.zeros(helper.spherical_harmonics._num_values(max_band))
+    data[0] = 1
 
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+    # dummy
+    phi = np.array([0, 0, 0, 0, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2])
+    theta = np.array([
+        np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2, np.pi / 2,
+        np.pi / 2, np.pi / 2
+    ])
+    data = helper.spherical_harmonics.real_spherical_harmonics(
+        phi + np.random.uniform(-0.5, 0.5, phi.size),
+        theta + np.random.uniform(-0.5, 0.5, phi.size), max_band)
 
+    if data.size > helper.spherical_harmonics._num_values(max_band):
+        raise ValueError("data band < vis band")
 
-fig = plt.figure()  # figsize=(10, 10)
-ax = fig.add_subplot(111, projection='3d')
+    odf = np.empty((n_phi, n_theta, 6), np.float32)
+    for phi_i, phi in enumerate(np.linspace(0, 2 * np.pi, n_phi, True)):
+        cosphi = np.cos(phi)
+        sinphi = np.sin(phi)
+        for theta_i, theta in enumerate(np.linspace(0, np.pi, n_theta, True)):
+            costheta = np.cos(theta)
+            sintheta = np.sin(theta)
 
-X = odf[:, :, 0]
-Y = odf[:, :, 1]
-Z = odf[:, :, 2]
+            r = 0
+            c = 0
+            for band in range(0, max_band + 1, 2):
+                for order in range(-band, band + 1):
+                    r += helper.spherical_harmonics._spherical_harmonics(
+                        band, order, costheta, sintheta, phi) * data[c]
+                    c += 1
 
-ax.plot_surface(X, Y, Z, facecolors=odf[:, :, 3:])
-set_axes_equal(ax)
-plt.show()
+            x = r * sintheta * cosphi
+            y = r * sintheta * sinphi
+            z = r * costheta
+            color = fastpli.analysis.images._orientation_to_hsv(
+                phi, np.pi / 2 - theta) / 255
+
+            odf[phi_i, theta_i, :] = [x, y, z, color[0], color[1], color[2]]
+
+    # to dat file
+    with open("odf_test.dat", "w") as file:
+        file.write(f"x y z rgb\n")
+        for i in range(odf.shape[0]):
+            for j in range(odf.shape[1]):
+                file.write(
+                    f"{odf[i,j,0]:.2f} {odf[i,j,1]:.2f} {odf[i,j,2]:.2f} {odf[i,j,3]:.2f},{odf[i,j,4]:.2f},{odf[i,j,5]:.2f}\n"
+                )
+            file.write("\n")
+
+    # plot
+    def set_axes_equal(ax):
+        x_limits = ax.get_xlim3d()
+        y_limits = ax.get_ylim3d()
+        z_limits = ax.get_zlim3d()
+
+        x_range = abs(x_limits[1] - x_limits[0])
+        x_middle = np.mean(x_limits)
+        y_range = abs(y_limits[1] - y_limits[0])
+        y_middle = np.mean(y_limits)
+        z_range = abs(z_limits[1] - z_limits[0])
+        z_middle = np.mean(z_limits)
+
+        plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+        ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+        ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+        ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+    fig = plt.figure()  # figsize=(10, 10)
+    ax = fig.add_subplot(111, projection='3d')
+
+    X = odf[:, :, 0]
+    Y = odf[:, :, 1]
+    Z = odf[:, :, 2]
+
+    ax.plot_surface(X, Y, Z, facecolors=odf[:, :, 3:])
+    set_axes_equal(ax)
+    plt.show()
