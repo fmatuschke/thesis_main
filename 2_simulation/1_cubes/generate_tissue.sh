a9fdf7666d36d55deebdffac4ad0915dd393ca54
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-29 11:03:44 +0100

    RFC output paths

--- 2_simulation/1_cubes/generate_tissue.sh
+++ 2_simulation/1_cubes/generate_tissue.sh
@@ -19,7 +19,7 @@ mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_30.00*.solved.h5 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_60.00*.solved.h5 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_90.00*.solved.h5 \
-   -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120_ime \
+   -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120_ime/tissue \
    -v 0.125 \
    --n_inc 4 \
    --d_rot 15
--- 2_simulation/1_cubes/simulation_ime.sh
+++ 2_simulation/1_cubes/simulation_ime.sh
@@ -20,7 +20,7 @@ mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_30.00*.solved.h5 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_60.00*.solved.h5 \
    /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_90.00*.solved.h5 \
-   -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120_ime_ \
+   -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120 \
    -v 0.125 \
    --start 0 \
    --n_inc 4 \
