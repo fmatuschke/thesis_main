c08afc0753f99a14e8b5e646de7ee079c3f71185
fmatuschke <f.matuschke@fz-juelich.de>
2021-01-29 13:49:34 +0100

    RFC generate_tissue

--- 2_simulation/1_cubes/generate_tissue.py
+++ 2_simulation/1_cubes/generate_tissue.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 import argparse
+import glob
 
 import os
 import warnings
@@ -15,42 +16,15 @@ import fastpli.io
 import models
 import helper.file
 
+import pretty_errors
+
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-o",
-                    "--output",
-                    type=str,
-                    required=True,
-                    help="Output path.")
-
-parser.add_argument("-i",
-                    "--input",
-                    nargs='+',
-                    required=True,
-                    help="input string.")
-
-parser.add_argument("-v",
-                    "--voxel_size",
-                    type=float,
-                    required=True,
-                    help="voxel_size in um.")
-
-parser.add_argument(
-    "--n_inc",  #4
-    type=int,
-    required=True,
-    help="number of fiber inclinations")
-
-parser.add_argument(
-    "--d_rot",  #15
-    type=int,
-    required=True,
-    help="number of fiber inclinations")
-
+parser.add_argument("-i", "--input", required=True, help="input string.")
 args = parser.parse_args()
-os.makedirs(args.output, exist_ok=True)
+os.makedirs(os.path.join(args.input, "tissue"), exist_ok=True)
 
 if __name__ == "__main__":
 
@@ -60,43 +34,24 @@ if __name__ == "__main__":
     LENGTH = 60
     THICKNESS = 60
 
-    file_list = args.input
+    file_list = glob.glob(os.path.join(args.input, '*.h5'))
 
-    # simulation loop
-    parameter = []
-    fiber_inc = [
-        (f, i) for f in file_list for i in models.inclinations(args.n_inc)
-    ]
-    for file, f0_inc in fiber_inc:
-        omega = helper.file.value(file, "omega")
+    for file in tqdm.tqdm(file_list[comm.Get_rank()::comm.Get_size()]):
 
-        for f1_rot in models.omega_rotations(omega, args.d_rot):
-            parameter.append((file, f0_inc, f1_rot))
-
-    for file, f0_inc, f1_rot in tqdm.tqdm(
-            parameter[comm.Get_rank()::comm.Get_size()]):
-
-        _, file_name = os.path.split(file)
+        file_path, file_name = os.path.split(file)
         file_name = os.path.splitext(file_name)[0]
-        file_name += f'_vs_{args.voxel_size:.4f}'
-        file_name += f'_inc_{f0_inc:.2f}'
-        file_name += f'_rot_{f1_rot:.2f}'
-        file_name = os.path.join(args.output, file_name)
-
-        if not os.path.isfile(file_name + '.h5'):
-            print(f"file: {file_name}.h5 does not exist")
-            exit(1)
-
-        # if os.path.isfile(file_name + '.tissue.h5'):
-        #     print(f"file: {file_name}.tissue.h5 already exists")
-        #     continue
+        file_name = os.path.join(file_path, "tissue", file_name)
 
         with h5py.File(file, 'r') as h5f:
-            fiber_bundles = fastpli.io.fiber_bundles.load_h5(h5f)
-            psi = h5f['/'].attrs["psi"]
-            omega = h5f['/'].attrs["omega"]
-            radius = h5f['/'].attrs["radius"]
-            v0 = h5f['/'].attrs["v0"]
+            dset = h5f['PM/Vervet/p']
+            fiber_bundles = fastpli.io.fiber_bundles.load(
+                dset.attrs['parameter/fiber_path'])
+            psi = dset.attrs["parameter/psi"]
+            omega = dset.attrs["parameter/omega"]
+            radius = dset.attrs["parameter/radius"]
+            f0_inc = dset.attrs["parameter/f0_inc"]
+            f1_rot = dset.attrs["parameter/f1_rot"]
+        voxel_size = helper.file.value(file, "vs")
 
         rot_inc = fastpli.tools.rotation.y(-np.deg2rad(f0_inc))
         rot_phi = fastpli.tools.rotation.x(np.deg2rad(f1_rot))
@@ -115,7 +70,7 @@ if __name__ == "__main__":
                 simpli = fastpli.simulation.Simpli()
                 warnings.filterwarnings("ignore", message="objects overlap")
                 simpli.omp_num_threads = 1
-                simpli.voxel_size = args.voxel_size
+                simpli.voxel_size = voxel_size
                 simpli.filter_rotations = np.linspace(0, np.pi, 9, False)
                 simpli.interpolate = "Slerp"
                 simpli.wavelength = 525  # in nm
@@ -140,35 +95,14 @@ if __name__ == "__main__":
                                           compression_opts=4)
 
                 dset[:] = tissue
-                # h5f[f"/tissue_max_proj"] = np.max(tissue,
-                #                                   axis=-1).astype(np.uint16)
-                # h5f[f"/tissue_mean_proj"] = np.mean(tissue,
-                #                                     axis=-1).astype(np.float32)
-                # h5f[f"/tissue_sum_proj"] = np.sum(tissue,
-                #                                   axis=-1).astype(np.uint16)
 
-                tissue = tissue.astype(np.float32)
-                tmp = tissue.ravel()
-                tmp[tmp > 0] = ((tmp[tmp > 0] - 1) // 2) + 1
-                tmp[tmp == 0] = np.nan
+                # tissue = tissue.astype(np.float32)
+                # tmp = tissue.ravel()
+                # tmp[tmp > 0] = ((tmp[tmp > 0] - 1) // 2) + 1
+                # tmp[tmp == 0] = np.nan
 
-                with warnings.catch_warnings():
-                    warnings.simplefilter(
-                        "ignore", message='RuntimeWarning: Mean of empty slice')
-                    h5f[f"/tissue_bin_median"] = np.nanmedian(tissue, axis=-1)
-                    h5f[f"/tissue_bin_mean"] = np.nanmean(tissue, axis=-1)
-
-                # dset = h5f.create_dataset('tissue_bin',
-                #                           tissue.shape,
-                #                           dtype=np.uint16,
-                #                           compression='gzip',
-                #                           compression_opts=4)
-
-                # dset[:] = tissue
-
-                # h5f[f"/tissue_bin_max_proj"] = np.max(tissue,
-                #                                       axis=-1).astype(np.uint16)
-                # h5f[f"/tissue_bin_mean_proj"] = np.mean(tissue, axis=-1).astype(
-                #     np.float32)
-                # h5f[f"/tissue_bin_sum_proj"] = np.sum(tissue,
-                #                                       axis=-1).astype(np.uint16)
+                # with warnings.catch_warnings():
+                #     warnings.filterwarnings(
+                #         "ignore", message='RuntimeWarning: Mean of empty slice')
+                #     h5f[f"/tissue_bin_median"] = np.nanmedian(tissue, axis=-1)
+                #     h5f[f"/tissue_bin_mean"] = np.nanmean(tissue, axis=-1)
--- 2_simulation/1_cubes/generate_tissue.sh
+++ 2_simulation/1_cubes/generate_tissue.sh
@@ -1,25 +1,6 @@
 #!/bin/bash -x
 
-mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
+mpirun -n 48 \
+   /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    -m mpi4py generate_tissue.py \
-   -i /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.00*omega_0.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.30*omega_0.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.30*omega_30.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.30*omega_60.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.30*omega_90.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.50*omega_0.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.50*omega_30.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.50*omega_60.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.50*omega_90.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.60*omega_0.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.60*omega_30.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.60*omega_60.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.60*omega_90.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_0.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_30.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_60.00*.solved.h5 \
-   /data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120/*psi_0.90*omega_90.00*.solved.h5 \
-   -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120_ime/tissue \
-   -v 0.125 \
-   --n_inc 4 \
-   --d_rot 15
+   -i /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/sim_120_ime
