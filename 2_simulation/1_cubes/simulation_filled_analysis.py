ec23482e59a26388b2169b051f59cdea562b899b
fmatuschke <f.matuschke@fz-juelich.de>
2021-03-05 10:11:43 +0100

    ADD py script

--- 2_simulation/1_cubes/simulation_filled_analysis.py
+++ 2_simulation/1_cubes/simulation_filled_analysis.py
@@ -1,95 +1,61 @@
-import numpy as np
-import h5py
-import argparse
-import logging
-import subprocess
-import glob
-import sys
 import os
 
-import warnings
-
-import fastpli.simulation
-import fastpli.analysis
-import fastpli.objects
-import fastpli.tools
-import fastpli.io
-
-import simulation_filled_helper
-
+import numpy as np
+import pandas as pd
 import tqdm
 
-# from simulation_repeat import run_simulation_pipeline_n
-import helper.mpi
-import helper.file
-import models
+import helper.spherical_harmonics
+import helper.schilling
+import polar_hist_to_tikz
 
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-import multiprocessing as mp
+path = 'output/sim_120_ime_filled'
+df = pd.read_pickle(os.path.join(path, "analysis", f"cube_2pop_simulation.pkl"))
+df["trel_mean"] = df["rofl_trel"].apply(lambda x: np.mean(x))
+df["ret_mean"] = df["epa_ret"].apply(lambda x: np.mean(x))
+df["trans_mean"] = df["epa_trans"].apply(lambda x: np.mean(x))
 
-# reproducability
-np.random.seed(42)
+microscope = 'PM'
+species = 'Vervet'
+df = df.query('microscope == @microscope & species == @species')
+df_ = []
+for index, row in tqdm.tqdm(df[df.model == 'r'].iterrows(),
+                            total=len(df[df.model == 'r'])):
+    radius, omega, psi, f0_inc, f1_rot = row['radius'], row['omega'], row[
+        'psi'], row['f0_inc'], row['f1_rot']
 
-parser = argparse.ArgumentParser()
+    phi = row['rofl_dir'].ravel()
+    theta = np.pi / 2 - row['rofl_inc'].ravel()
+    sh_filled = helper.spherical_harmonics.real_spherical_harmonics(
+        phi, theta, 6)
 
-parser.add_argument("-i",
-                    "--input",
-                    type=str,
-                    required=True,
-                    help="input path.")
-parser.add_argument("-f",
-                    "--filled",
-                    type=str,
-                    required=True,
-                    help="filled path.")
-parser.add_argument("-p",
-                    "--num_proc",
-                    default=1,
-                    type=int,
-                    help="Number of processes.")
+    phi = df.iloc[index + 1]['rofl_dir'].ravel()
+    theta = np.pi / 2 - df.iloc[index + 1]['rofl_inc'].ravel()
+    sh = helper.spherical_harmonics.real_spherical_harmonics(phi, theta, 6)
 
-args = parser.parse_args()
+    df_.append(
+        pd.DataFrame(
+            {
+                "radius":
+                    radius,
+                "omega":
+                    omega,
+                "psi":
+                    psi,
+                "f0_inc":
+                    f0_inc,
+                "f1_rot":
+                    f1_rot,
+                "acc":
+                    helper.schilling.angular_correlation_coefficient(
+                        sh, sh_filled)
+            },
+            index=[0]))
+df_ = pd.concat(df_, ignore_index=True)
 
-df_in = pd.read_pickle(
-    os.path.join(args.input, "analysis", f"cube_2pop_simulation.pkl"))
-df_in["trel_mean"] = df_in["rofl_trel"].apply(lambda x: np.mean(x))
-df_in["ret_mean"] = df_in["epa_ret"].apply(lambda x: np.mean(x))
-df_in["trans_mean"] = df_in["epa_trans"].apply(lambda x: np.mean(x))
-
-df_fill = pd.read_pickle(
-    os.path.join(args.filled, "analysis", f"cube_2pop_simulation.pkl"))
-df_fill["trel_mean"] = df_fill["rofl_trel"].apply(lambda x: np.mean(x))
-df_fill["ret_mean"] = df_fill["epa_ret"].apply(lambda x: np.mean(x))
-df_fill["trans_mean"] = df_fill["epa_trans"].apply(lambda x: np.mean(x))
-
-# def run(p):
-#     radius = p[1].radius
-#     microscope = p[1].microscope
-#     species = p[1].species
-#     model = p[1].model
-
-#     # ACC
-#     sub = (df_acc.radius == radius) & (df_acc.microscope == microscope) & (
-#         df_acc.species == species) & (df_acc.model == model)
-
-# if __name__ == "__main__":
-
-#     df_p = df[[
-#         "radius",
-#         "microscope",
-#         "species",
-#         "model",
-#     ]].drop_duplicates()
-
-#     df_p = df_p[df_p.radius == 0.5]
-#     df_p = df_p[df_p.species == "Vervet"]
-#     df_p = df_p[df_p.model == "r"]
-#     df_p = df_p[df_p.microscope == "PM"]
-
-#     with mp.Pool(processes=args.num_proc) as pool:
-#         [
-#             _ for _ in tqdm.tqdm(pool.imap_unordered(run, df_p.iterrows()),
-#                                  total=len(df_p),
-#                                  smoothing=0.1)
-#         ]
+for radius in df_.radius.unique():
+    polar_hist_to_tikz.generate(df_[df_.radius == radius],
+                                "acc",
+                                f"simulation_analysis_filled_hist_{radius}_acc",
+                                crange=[0, 1],
+                                psi_list=[0.30, 0.50, 0.60, 0.90],
+                                f0_list=[0, 30, 60, 90])
