f2fed2cf2f705bc2ce0bb51f0f30bad4fd457bfb
fmatuschke <f.matuschke@fz-juelich.de>
2021-04-12 15:36:56 +0200

    RFC

--- 2_simulation/1_cubes/simulation_ime.sh
+++ 2_simulation/1_cubes/simulation_ime.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -x
 
-NAME=sim_120_newest
+NAME=sim_cube_2pop_120
 MODEL_PATH=/data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/cube_2pop_120
 
 # Vorauswahl
 mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    -m mpi4py simulation_ime.py \
-   -i \
-   ${MODEL_PATH}/*psi_0.[03569]0*omega_0.00*.solved.h5 \
+   -i ${MODEL_PATH}/*psi_0.[03569]0*omega_0.00*.solved.h5 \
    ${MODEL_PATH}/*psi_0.[03569]0*omega_[369]0.00*.solved.h5 \
    -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/${NAME} \
    -v 0.125 \
