6473f28c8339f38af1600c363e1b8f46ba777143
fmatuschke <f.matuschke@fz-juelich.de>
2021-04-27 12:08:24 +0200

    RFC model path

--- 1_model/0_parameter/pre_stats_box_plot.sh
+++ 1_model/0_parameter/pre_stats_box_plot.sh
@@ -5,12 +5,12 @@ mkdir -p output/tmp
 mkdir -p output/tmp/output/tmp
 mkdir -p output/tikz
 
-# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp parameter_statistic_box_plot.tex
-# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp parameter_statistic_box_plot.tex
-# mv output/tmp/parameter_statistic_box_plot.pdf output/tikz/parameter_statistic_box_plot.pdf
-# xdg-open output/tikz/parameter_statistic_box_plot.pdf &>/dev/null 2>&1
+# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp pre_stats_box_plot.tex
+# lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp pre_stats_box_plot.tex
+# mv output/tmp/pre_stats_box_plot.pdf output/tikz/pre_stats_box_plot.pdf
+# xdg-open output/tikz/pre_stats_box_plot.pdf &>/dev/null 2>&1
 
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp parameter_statistic_box_plot_volume.tex
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp parameter_statistic_box_plot_volume.tex
-mv output/tmp/parameter_statistic_box_plot_volume.pdf output/tikz/parameter_statistic_box_plot_volume.pdf
-xdg-open output/tikz/parameter_statistic_box_plot_volume.pdf &>/dev/null 2>&1
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp pre_stats_box_plot_volume.tex
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/tmp pre_stats_box_plot_volume.tex
+mv output/tmp/pre_stats_box_plot_volume.pdf output/tikz/parameter_statistic_box_plot_volume.pdf
+xdg-open output/tikz/pre_stats_box_plot_volume.pdf &>/dev/null 2>&1
