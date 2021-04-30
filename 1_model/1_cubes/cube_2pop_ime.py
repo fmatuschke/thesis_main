716ce8778a081139e46f59db0bf8c0faaae05e2f
fmatuschke <f.matuschke@fz-juelich.de>
2021-04-30 09:53:50 +0200

    ADD hostname to output path, RM executable py

--- 1_model/1_cubes/cube_2pop_ime.py
+++ 1_model/1_cubes/cube_2pop_ime.py
@@ -4,6 +4,8 @@ import os
 
 import tqdm
 
+HOST = os.uname()[1]
+
 
 def run(p):
     radius = p[0]
@@ -13,18 +15,18 @@ def run(p):
     n_thread = p[4]
     start = p[5]
     subprocess.call([
-        "/data/PLI-Group/felix/data/thesis/env-ime656/bin/python3",
+        f"/data/PLI-Group/felix/data/thesis/env-{HOST}/bin/python3",
         "cube_2pop.py", "-o",
-        os.path.join(output, f"cube_2pop_{volume}"), "-r", f"{radius}", "-v",
-        f"{volume}", "-n", f"{n_max}", "-p", f"{n_thread}", "--start",
-        f"{start}"
+        os.path.join(output, f"cube_2pop_{volume}_rc1_{HOST}"), "-r",
+        f"{radius}", "-v", f"{volume}", "-n", f"{n_max}", "-p", f"{n_thread}",
+        "--start", f"{start}"
     ])
 
 
 N = 92
 with mp.Pool(48) as pool:
     for r in [10, 5, 2, 1, 0.5]:
-        parameters = [(r, 135, "output", 10000, 1, s) for s in range(N)]
+        parameters = [(r, 135, "output", 100000, 1, s) for s in range(N)]
         print(f"radius: {r}")
         for _ in pool.imap_unordered(run, parameters):
             pass
