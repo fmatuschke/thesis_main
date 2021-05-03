43eea570d57b0a8a654d04e9d5cbc7ebfebe917a
fmatuschke <f.matuschke@fz-juelich.de>
2021-05-03 09:27:49 +0200

    ADD NAME tag

--- 1_model/1_cubes/cube_2pop_ime.py
+++ 1_model/1_cubes/cube_2pop_ime.py
@@ -4,6 +4,7 @@ import os
 
 import tqdm
 
+NAME = 'rc1.1'
 HOST = os.uname()[1]
 PATH = os.path.dirname(os.path.realpath(__file__))
 THESIS = os.path.join(os.path.realpath(__file__).split('/thesis/')[0], 'thesis')
@@ -18,7 +19,7 @@ def run(p):
     index = p[5]
     subprocess.call([
         os.path.join(THESIS, f"env-{HOST}/bin/python3"), "cube_2pop.py", "-o",
-        os.path.join(output, f"cube_2pop_{volume}_rc1"), "-r", f"{radius}",
+        os.path.join(output, f"cube_2pop_{volume}_{NAME}"), "-r", f"{radius}",
         "-v", f"{volume}", "-n", f"{n_max}", "-p", f"{n_thread}", "--index",
         f"{index}"
     ])
@@ -28,6 +29,7 @@ N = 92
 with mp.Pool(mp.cpu_count() // 2) as pool:
     for r in [10, 5, 2, 1, 0.5]:
         parameters = [(r, 135, "output", 100000, 1, s) for s in range(N)]
-        print(f"radius: {r}")
-        for _ in pool.imap_unordered(run, parameters):
+        for _ in tqdm.tqdm(pool.imap_unordered(run, parameters),
+                           total=len(parameters),
+                           desc=f"radius: {r}"):
             pass
