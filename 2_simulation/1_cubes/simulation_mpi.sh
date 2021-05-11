40295d1091ca6b061ec79d049fd244bfee637d4c
fmatuschke <f.matuschke@fz-juelich.de>
2021-05-11 07:50:28 +0200

    RFC mpi=48, omp=1

--- 2_simulation/1_cubes/simulation_mpi.sh
+++ 2_simulation/1_cubes/simulation_mpi.sh
@@ -5,7 +5,7 @@ NAME=cube_2pop_135_rc1
 MODEL_PATH=/data/PLI-Group/felix/data/thesis/1_model/1_cubes/output/$NAME
 
 # Vorauswahl
-mpirun -n 33 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
+mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    -m mpi4py.futures simulation_mpi.py \
    -i ${MODEL_PATH}/*psi_0.[03569]0*omega_0.00*.solved.h5 \
    ${MODEL_PATH}/*psi_0.[03569]0*omega_[369]0.00*.solved.h5 \
@@ -16,7 +16,7 @@ python3 simulation_post_1.py -i /data/PLI-Group/felix/data/thesis/2_simulation/1
 python3 simulation_post_2.py -i /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/${NAME} -p 48
 #
 # radien eliminierung
-mpirun -n 33 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
+mpirun -n 48 /data/PLI-Group/felix/data/thesis/env-$(hostname)/bin/python3 \
    -m mpi4py.futures simulation_mpi.py \
    -i ${MODEL_PATH}/*r_0.50*.solved.h5 \
    -o /data/PLI-Group/felix/data/thesis/2_simulation/1_cubes/output/${NAME}_r_0.5 \
